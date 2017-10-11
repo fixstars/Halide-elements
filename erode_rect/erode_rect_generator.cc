7ba57f6e98e7f3c72f35bc05614e1c80a3330a26
takashi.osawa <takashi.osawa@fixstars.com>
2017-10-11 10:54:01 +0900

    1st drop of erode_rect filter

---
+++ erode_rect/erode_rect_generator.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "Halide.h"
+
+using namespace Halide;
+
+class ErodeRect : public Halide::Generator<ErodeRect> {
+public:
+    GeneratorParam<int32_t> width{"width", 1024};
+    GeneratorParam<int32_t> height{"height", 768};
+    GeneratorParam<uint32_t> iteration{"iteration", 2};
+    ImageParam src{UInt(8), 2, "src"};
+    Param<uint32_t> window_width{"window_width"};
+    Param<uint32_t> window_height{"window_height"};
+
+    Var x, y;
+
+    Func build() {
+
+	Func input("input");
+	input(x, y) = src(x, y);
+	
+	RDom r(-(window_width / 2), window_width, -(window_height / 2), window_height);
+	for (int i = 0; i < iteration; i++) {
+	    Func clamped = BoundaryConditions::repeat_edge(input, {{0, cast<int32_t>(width)}, {0, cast<int32_t>(height)}});
+	    Func workbuf("workbuf");
+	    Expr val = minimum(clamped(x + r.x, y + r.y));
+	    workbuf(x, y) = val;
+	    workbuf.compute_root();
+	    input = workbuf;
+	}
+
+	return input;
+    }
+};
+
+RegisterGenerator<ErodeRect> erode_rect{"erode_rect"};
