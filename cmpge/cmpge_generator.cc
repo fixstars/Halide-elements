928ae2ff22069c2bd60a19ac461eb32612d97446
takashi.osawa <takashi.osawa@fixstars.com>
2017-10-11 11:07:35 +0900

    1st drop of cmpge filter

---
+++ cmpge/cmpge_generator.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <climits>
+#include "Halide.h"
+
+using namespace Halide;
+
+class Cmpge : public Halide::Generator<Cmpge> {
+public:
+    GeneratorParam<int32_t> width{"width", 1024};
+    GeneratorParam<int32_t> height{"height", 768};
+    ImageParam src0{UInt(8), 2, "src0"}, src1{UInt(8), 2, "src1"};
+
+    Var x, y;
+
+    Func build() {
+
+        Func dst("dst");
+	Expr srcval0 = src0(x, y), srcval1 = src1(x, y);
+	Expr dstval = cast<uint8_t>(srcval0 >= srcval1);
+        dst(x, y) = dstval * UCHAR_MAX;
+        
+        return dst;
+    }
+};
+
+RegisterGenerator<Cmpge> cmpge{"cmpge"};
