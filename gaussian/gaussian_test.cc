3a5fbd662c3ed555f755aabec06e1784fbf5a5d8
takashi.osawa <takashi.osawa@fixstars.com>
2017-10-12 14:34:30 +0900

    gaussian: modify neareset even

--- gaussian/gaussian_test.cc
+++ gaussian/gaussian_test.cc
@@ -8,23 +8,10 @@
 
 #include "gaussian.h"
 
+#include "testcommon.h"
+
 using std::string;
-using std::vector;
 using Halide::Runtime::Buffer;
-
-namespace {
-
-template<typename... Rest>
-string format(const char *fmt, const Rest&... rest)
-{
-    int length = snprintf(NULL, 0, fmt, rest...) + 1; // Explicit place for null termination
-    vector<char> buf(length, 0);
-    snprintf(&buf[0], length, fmt, rest...);
-    string s(buf.begin(), std::find(buf.begin(), buf.end(), '\0'));
-    return s;
-}
-
-}
 
 int main()
 {
@@ -36,44 +23,45 @@ int main()
         //
         const int width = 1024;
         const int height = 768;
-	const int window_width = 3;
-	const int window_height = 3;
-	const float sigma = 1.0;
+        const int window_width = 3;
+        const int window_height = 3;
+        const float sigma = 1.0;
         Buffer<uint8_t> input(width, height);
         Buffer<uint8_t> output(width, height);
+        std::srand(time(NULL));
         for (int y=0; y<height; ++y) {
             for (int x=0; x<width; ++x) {
-                input(x, y) = (uint8_t)(x + y);
+                input(x, y) = static_cast<uint8_t>(std::rand());
                 output(x, y) = 0;
             }
         }
 
         gaussian(input, window_width, window_height, sigma, output);
 
-	float kernel_sum = 0;
-	for (int i = -(window_width/2); i < -(window_width/2) + window_width; i++) {
-	    for (int j = -(window_height/2); j < -(window_height/2) + window_height; j++) {
-		kernel_sum += exp(-(i * i + j * j) / (2 * sigma * sigma));
-	    }
-	}
+        float kernel_sum = 0;
+        for (int i = -(window_width/2); i < -(window_width/2) + window_width; i++) {
+            for (int j = -(window_height/2); j < -(window_height/2) + window_height; j++) {
+                kernel_sum += exp(-(i * i + j * j) / (2 * sigma * sigma));
+            }
+        }
 
         for (int y=0; y<height; ++y) {
             for (int x=0; x<width; ++x) {
-		float expect_f = 0.0f;
-		for (int j = -(window_height/2); j < -(window_height/2) + window_height; j++) {
-		    int yy = y + j >= 0 ? y + j: 0;
-		    yy = yy < height ? yy : height - 1;
-		    for (int i = -(window_width/2); i < -(window_width/2) + window_width; i++) {
-			int xx = x + i >= 0 ? x + i: 0;
-			xx = xx < width ? xx : width - 1;
-			expect_f += exp(-(i * i + j * j) / (2 * sigma * sigma)) * input(xx, yy);
-		    }
-		}
-		expect_f /= kernel_sum;
-		uint8_t expect = (uint8_t)(expect_f + 0.5f);
+                float expect_f = 0.0f;
+                for (int j = -(window_height/2); j < -(window_height/2) + window_height; j++) {
+                    int yy = y + j >= 0 ? y + j: 0;
+                    yy = yy < height ? yy : height - 1;
+                    for (int i = -(window_width/2); i < -(window_width/2) + window_width; i++) {
+                        int xx = x + i >= 0 ? x + i: 0;
+                        xx = xx < width ? xx : width - 1;
+                        expect_f += exp(-(i * i + j * j) / (2 * sigma * sigma)) * input(xx, yy);
+                    }
+                }
+                expect_f /= kernel_sum;
+                uint8_t expect = round_to_nearest_even<uint8_t>(expect_f);
                 uint8_t actual = output(x, y);
                 if (expect != actual) {
-                    throw std::runtime_error(format("Error: expect(%d, %d) = %d, actual(%d, %d) = %d", x, y, expect, x, y, actual).c_str());
+                    throw std::runtime_error(format("Error: expect(%d, %d) = %d, actual(%d, %d) = %d, expect_f = %f", x, y, expect, x, y, actual, expect_f).c_str());
                 }
             }
         }
