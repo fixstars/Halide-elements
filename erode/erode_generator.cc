0bae7bc1da1809aa8b093cb768b06211e512172f
takashi.osawa <takashi.osawa@fixstars.com>
2017-10-12 14:25:46 +0900

    erode: modifyt to use select, set_range

--- erode/erode_generator.cc
+++ erode/erode_generator.cc
@@ -7,34 +7,33 @@ class Erode : public Halide::Generator<Erode> {
 public:
     GeneratorParam<int32_t> width{"width", 1024};
     GeneratorParam<int32_t> height{"height", 768};
-    GeneratorParam<uint32_t> iteration{"iteration", 2};
+    GeneratorParam<int32_t> iteration{"iteration", 2};
     ImageParam src{UInt(8), 2, "src"};
-    ImageParam structure{UInt(8), 1, "structure"};
-    Param<uint32_t> window_width{"window_width"};
-    Param<uint32_t> window_height{"window_height"};
+    ImageParam structure{UInt(8), 2, "structure"};
+    Param<int32_t> window_width{"window_width"};
+    Param<int32_t> window_height{"window_height"};
 
     Var x, y;
 
     Func build() {
-	
-	Func input("input");
-	input(x, y) = src(x, y);
 
-	RDom r(-(window_width / 2), window_width, -(window_height / 2), window_height);
-	Func invkval("invkval");
-	invkval(x, y) = cast<uint8_t>(0);
-	invkval(r.x, r.y) = ~structure(window_width / 2 + r.x + window_width * (window_height / 2 + r.y));
-	invkval.compute_root();
-	for (int i = 0; i < iteration; i++) {
-	    Func clamped = BoundaryConditions::repeat_edge(input, {{0, cast<int32_t>(width)}, {0, cast<int32_t>(height)}});
-	    Func workbuf("workbuf");
-	    Expr val = cast<uint8_t>(minimum(cast<uint32_t>(clamped(x + r.x, y + r.y)) + cast<uint32_t>(invkval(r.x, r.y)) + cast<uint32_t>(1) - cast<uint32_t>(cast<uint8_t>(invkval(r.x, r.y)) + cast<uint8_t>(1))));
-	    workbuf(x, y) = val;
-	    workbuf.compute_root();
-	    input = workbuf;
-	}
+        window_width.set_range(3, 17);
+        window_height.set_range(3, 17);
+        
+        Func input("input");
+        input(x, y) = src(x, y);
 
-	return input;
+        RDom r(-(window_width / 2), window_width, -(window_height / 2), window_height);
+        for (int32_t i = 0; i < iteration; i++) {
+            Func clamped = BoundaryConditions::repeat_edge(input, {{0, cast<int32_t>(width)}, {0, cast<int32_t>(height)}});
+            Func workbuf("workbuf");
+            Expr val = minimum(select(structure(r.x + window_width / 2, r.y + window_height / 2) == 0, type_of<uint8_t>().max(), clamped(x + r.x, y + r.y)));
+            workbuf(x, y) = val;
+            workbuf.compute_root();
+            input = workbuf;
+        }
+        
+        return input;
     }
 };
 
