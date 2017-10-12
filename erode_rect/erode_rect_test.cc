442a9410f3d4f8d06c8f83850d1986f2d8f671d6
takashi.osawa <takashi.osawa@fixstars.com>
2017-10-12 14:33:07 +0900

    erode_rect: modify to use set_range

--- erode_rect/erode_rect_test.cc
+++ erode_rect/erode_rect_test.cc
@@ -9,23 +9,10 @@
 
 #include "erode_rect.h"
 
+#include "testcommon.h"
+
 using std::string;
-using std::vector;
 using Halide::Runtime::Buffer;
-
-namespace {
-
-template<typename... Rest>
-string format(const char *fmt, const Rest&... rest)
-{
-    int length = snprintf(NULL, 0, fmt, rest...) + 1; // Explicit place for null termination
-    vector<char> buf(length, 0);
-    snprintf(&buf[0], length, fmt, rest...);
-    string s(buf.begin(), std::find(buf.begin(), buf.end(), '\0'));
-    return s;
-}
-
-}
 
 int main()
 {
@@ -37,52 +24,54 @@ int main()
         //
         const int width = 1024;
         const int height = 768;
-	const int window_width = 3;
-	const int window_height = 3;
-	const int iteration = 2;
+        const int window_width = 3;
+        const int window_height = 3;
+        const int iteration = 2;
         Buffer<uint8_t> input(width, height);
         Buffer<uint8_t> output(width, height);
-	uint8_t expect[width][height], workbuf[width][height];
+        uint8_t (*expect)[width][height], workbuf[2][width][height];
+        std::srand(time(NULL));
         for (int y=0; y<height; ++y) {
             for (int x=0; x<width; ++x) {
-		input(x, y) = (uint8_t)(x + y);
-		workbuf[x][y] = input(x, y);
-                output(x, y) = UCHAR_MAX;
+                input(x, y) = static_cast<uint8_t>(std::rand());
+                workbuf[0][x][y] = input(x, y);
+                output(x, y) = std::numeric_limits<uint8_t>::max();
             }
         }
 
-	for (int k=0; k<iteration; ++k) {
-	    for (int y=0; y<height; ++y) {
-		for (int x=0; x<width; ++x) {
-		    uint8_t min = UCHAR_MAX;
-		    for (int j = -(window_height/2); j < -(window_height/2) + window_height; j++) {
-			int yy = y + j >= 0 ? y + j: 0;
-			yy = yy < height ? yy : height - 1;
-			for (int i = -(window_width/2); i < -(window_width/2) + window_width; i++) {
-			    int xx = x + i >= 0 ? x + i: 0;
-			    xx = xx < width ? xx : width - 1;
-			    if (min > workbuf[xx][yy]) {
-				min = workbuf[xx][yy];
-			    }
-			}
-		    }
-		    expect[x][y] = min;
-		}
-	    }
-	    memcpy(workbuf, expect, sizeof(workbuf));
-	}
-	
+        int k;
+        for (k=0; k<iteration; ++k) {
+            for (int y=0; y<height; ++y) {
+                for (int x=0; x<width; ++x) {
+                    uint8_t min = std::numeric_limits<uint8_t>::max();
+                    for (int j = -(window_height/2); j < -(window_height/2) + window_height; j++) {
+                        int yy = y + j >= 0 ? y + j: 0;
+                        yy = yy < height ? yy : height - 1;
+                        for (int i = -(window_width/2); i < -(window_width/2) + window_width; i++) {
+                            int xx = x + i >= 0 ? x + i: 0;
+                            xx = xx < width ? xx : width - 1;
+                            if (min > workbuf[k%2][xx][yy]) {
+                                min = workbuf[k%2][xx][yy];
+                            }
+                        }
+                    }
+                    workbuf[(k+1)%2][x][y] = min;
+                }
+            }
+        }
+        expect = &(workbuf[k%2]);
+        
         erode_rect(input, window_width, window_height, output);
 
         for (int y=0; y<height; ++y) {
             for (int x=0; x<width; ++x) {
                 uint8_t actual = output(x, y);
-                if (expect[x][y] != actual) {
+                if ((*expect)[x][y] != actual) {
                     throw std::runtime_error(format("Error: expect(%d, %d) = %d, actual(%d, %d) = %d", x, y, expect[x][y], x, y, actual).c_str());
                 }
             }
         }
-
+        
     } catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
         return 1;
