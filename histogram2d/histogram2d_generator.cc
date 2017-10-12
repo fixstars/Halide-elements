ee553fd799043342927a23c6b0ff981e9c3d5b0a
takashi.osawa <takashi.osawa@fixstars.com>
2017-10-12 14:46:12 +0900

    histogram2d: modify test to use histogram location logic of roca implementation

--- histogram2d/histogram2d_generator.cc
+++ histogram2d/histogram2d_generator.cc
@@ -8,7 +8,7 @@ class Histogram2D : public Halide::Generator<Histogram2D> {
 public:
     GeneratorParam<int32_t> width{"width", 1024};
     GeneratorParam<int32_t> height{"height", 768};
-    GeneratorParam<int32_t> hist_width{"hist_width", UCHAR_MAX + 1};
+    GeneratorParam<int32_t> hist_width{"hist_width", std::numeric_limits<uint8_t>::max() + 1};
     ImageParam src0{UInt(8), 2, "src0"}, src1{UInt(8), 2, "src1"};
 
     Var x, y;
@@ -16,11 +16,11 @@ public:
     Func build() {
 
         Func dst("dst");
-	dst(x, y) = cast<uint32_t>(0);
-	RDom r(0, width, 0, height);
-	Expr idx0 = cast<int32_t>(src0(r.x, r.y) * cast<int32_t>(hist_width - 1) / cast<int32_t>(UCHAR_MAX));
-	Expr idx1 = cast<int32_t>(src1(r.x, r.y) * cast<int32_t>(hist_width - 1) / cast<int32_t>(UCHAR_MAX));
-	dst(idx0, idx1) += cast<uint32_t>(1);
+        dst(x, y) = cast<uint32_t>(0);
+        RDom r(0, width, 0, height);
+        Expr idx0 = cast<int32_t>(src0(r.x, r.y) * cast<uint32_t>(hist_width) / cast<uint32_t>(std::numeric_limits<uint8_t>::max() + 1));
+        Expr idx1 = cast<int32_t>(src1(r.x, r.y) * cast<uint32_t>(hist_width) / cast<uint32_t>(std::numeric_limits<uint8_t>::max() + 1));
+        dst(idx0, idx1) += cast<uint32_t>(1);
         
         return dst;
     }
--- histogram2d/histogram2d_test.cc
+++ histogram2d/histogram2d_test.cc
@@ -9,23 +9,10 @@
 
 #include "histogram2d.h"
 
+#include "testcommon.h"
+
 using std::string;
-using std::vector;
 using Halide::Runtime::Buffer;
-
-namespace {
-
-template<typename... Rest>
-string format(const char *fmt, const Rest&... rest)
-{
-    int length = snprintf(NULL, 0, fmt, rest...) + 1; // Explicit place for null termination
-    vector<char> buf(length, 0);
-    snprintf(&buf[0], length, fmt, rest...);
-    string s(buf.begin(), std::find(buf.begin(), buf.end(), '\0'));
-    return s;
-}
-
-}
 
 int main()
 {
@@ -36,43 +23,52 @@ int main()
         // Run
         //
         const int width = 1024;
-	const int height = 768;
-	const int hist_width = UCHAR_MAX + 1;
+        const int height = 768;
+        const int hist_width = std::numeric_limits<uint8_t>::max() + 1;
         Buffer<uint8_t> input0(width, height), input1(width, height);
         Buffer<uint32_t> output(hist_width, hist_width);
-	uint32_t expect[hist_width][hist_width];
-
-	memset(expect, 0, sizeof(expect));
+        uint32_t expect[hist_width][hist_width];
+        
+        std::srand(time(NULL));
+        memset(expect, 0, sizeof(expect));
+        double step =
+            hist_width / (static_cast<double>((std::numeric_limits<uint8_t>::max)()) + 1.0);
         for (int y=0; y<height; ++y) {
             for (int x=0; x<width; ++x) {
-                input0(x, y) = (uint8_t)(x + y);
-                input1(x, y) = (uint8_t)(x - y);		
-		expect[input0(x, y) * (hist_width - 1) / UCHAR_MAX][input1(x, y) * (hist_width - 1) / UCHAR_MAX]++;
+                input0(x, y) = static_cast<uint8_t>(std::rand());
+                input1(x, y) = static_cast<uint8_t>(std::rand());
+                int i_idx =
+                    static_cast<int>(std::floor((static_cast<double>(input0(x, y))) * step));
+                int j_idx =
+                    static_cast<int>(std::floor((static_cast<double>(input1(x, y))) * step));
+                if (i_idx < hist_width && j_idx < hist_width) {
+                    expect[i_idx][j_idx]++;
+                }
             }
         }
-
-	for (int y=0; y<hist_width; ++y) {
-	    for (int x=0; x<hist_width; ++x) {
-		output(x, y) = 0;
-	    }
-	}
-
+        
+        for (int y=0; y<hist_width; ++y) {
+            for (int x=0; x<hist_width; ++x) {
+                output(x, y) = 0;
+            }
+        }
+        
         histogram2d(input0, input1, output);
-
-	for (int y=0; y<hist_width; ++y) {
-	    for (int x=0; x<hist_width; ++x) {
-		uint32_t actual = output(x, y);
-		if (expect[x][y] != actual) {
-		    throw std::runtime_error(format("Error: expect(%d, %d) = %d, actual(%d, %d) = %d", x, y, expect[x][y], x, y, actual).c_str());
-		}
-	    }
-	}
-
+        
+        for (int y=0; y<hist_width; ++y) {
+            for (int x=0; x<hist_width; ++x) {
+                uint32_t actual = output(x, y);
+                if (expect[x][y] != actual) {
+                    throw std::runtime_error(format("Error: expect(%d, %d) = %d, actual(%d, %d) = %d", x, y, expect[x][y], x, y, actual).c_str());
+                }
+            }
+        }
+        
     } catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
         return 1;
     }
-
+    
     printf("Success!\n");
     return 0;
 }
