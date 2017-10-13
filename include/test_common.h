20c5ac8d387a378cde127c564f4fe72e50b06d95
Takuro Iizuka <t_iizuka@fixstars.com>
2017-10-12 17:27:17 -0700

    min/max of uniform_real_distribution

--- include/test_common.h
+++ include/test_common.h
@@ -40,14 +40,16 @@ Halide::Runtime::Buffer<T> mk_rand_int_buffer(const std::vector<int32_t>& extent
 }
 
 template<typename T>
-Halide::Runtime::Buffer<T> mk_rand_real_buffer(const std::vector<int32_t>& extents)
+Halide::Runtime::Buffer<T> mk_rand_real_buffer(const std::vector<int32_t>& extents, 
+                                               T min_value = std::numeric_limits<T>::min(), 
+                                               T max_value = std::numeric_limits<T>::max())
 {
     Halide::Runtime::Buffer<T> buf(extents);
     const int32_t size = std::accumulate(extents.begin(), extents.end(), 1, std::multiplies<int32_t>());
 
     std::random_device rd;
     std::mt19937 mt(rd());
-    std::uniform_real_distribution<T> dist(std::numeric_limits<T>::min(), std::numeric_limits<T>::max());
+    std::uniform_real_distribution<T> dist(min_value, max_value);
 
     for (int32_t i=0; i<size; ++i) {
         *(reinterpret_cast<T*>(buf.data())+i) = dist(mt);
