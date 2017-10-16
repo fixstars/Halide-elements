c073825df5e25eb22005655fab32396860e65994
takashi.osawa <takashi.osawa@fixstars.com>
2017-10-16 10:23:41 +0900

    rename LIST_TYPE to TYPE_LIST

--- roca_common.mk
+++ roca_common.mk
@@ -24,15 +24,15 @@ ${PROG}_gen: ${PROG}_generator.cc
 	g++ -fno-rtti ${CXXFLAGS} $< ${HALIDE_ROOT}/tools/GenGen.cpp -o ${PROG}_gen ${LIBS} -lHalide
 
 ${PROG}_gen.exec: ${PROG}_gen
-	$(foreach type,${LIST_TYPE},LD_LIBRARY_PATH=${HALIDE_LIB_DIR} ./${PROG}_gen -o . -g ${PROG}_${type} -e h,static_library target=host;)
+	$(foreach type,${TYPE_LIST},LD_LIBRARY_PATH=${HALIDE_LIB_DIR} ./${PROG}_gen -o . -g ${PROG}_${type} -e h,static_library target=host;)
 	@touch ${PROG}_gen.exec
 
-$(foreach type,${LIST_TYPE},${PROG}_${type}.a): ${PROG}_gen.exec
+$(foreach type,${TYPE_LIST},${PROG}_${type}.a): ${PROG}_gen.exec
 
-$(foreach type,${LIST_TYPE},${PROG}_${type}.h): ${PROG}_gen.exec
+$(foreach type,${TYPE_LIST},${PROG}_${type}.h): ${PROG}_gen.exec
 
-${PROG}_test: ${PROG}_test.cc $(foreach type,${LIST_TYPE},${PROG}_${type}.h ${PROG}_${type}.a)
-	g++ -I . ${CXXFLAGS} $< -o $@ $(foreach type,${LIST_TYPE},${PROG}_${type}.a) -ldl -lpthread
+${PROG}_test: ${PROG}_test.cc $(foreach type,${TYPE_LIST},${PROG}_${type}.h ${PROG}_${type}.a)
+	g++ -I . ${CXXFLAGS} $< -o $@ $(foreach type,${TYPE_LIST},${PROG}_${type}.a) -ldl -lpthread
 
 clean:
-	rm -rf ${PROG}_test ${PROG}_gen ${PROG}_gen.exec $(foreach type,${LIST_TYPE},${PROG}_${type}.h ${PROG}_${type}.a)
+	rm -rf ${PROG}_test ${PROG}_gen ${PROG}_gen.exec $(foreach type,${TYPE_LIST},${PROG}_${type}.h ${PROG}_${type}.a)
