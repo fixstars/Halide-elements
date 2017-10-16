1b3c8b67f1b76267e9245aa9c793a9a5cae6bfd6
takashi.osawa <takashi.osawa@fixstars.com>
2017-10-16 10:23:41 +0900

    rename LIST_TYPE to TYPE_LIST

--- roca_common.mk
+++ roca_common.mk
@@ -31,17 +31,15 @@ ${PROG}_gen: ${PROG}_generator.cc
 	g++ -fno-rtti ${CXXFLAGS} $< ${HALIDE_TOOLS_DIR}/GenGen.cpp -o ${PROG}_gen ${LIBS} -lHalide
 
 ${PROG}_gen.exec: ${PROG}_gen
-	LD_LIBRARY_PATH=${HALIDE_LIB_DIR} ./$< -o . -e h,static_library target=host-no_asserts
-=======
-	$(foreach type,${LIST_TYPE},LD_LIBRARY_PATH=${HALIDE_LIB_DIR} ./$< -o . -g ${PROG}_${type} -e h,static_library target=host-no-asserts;)
+	$(foreach type,${TYPE_LIST},LD_LIBRARY_PATH=${HALIDE_LIB_DIR} ./$< -o . -g ${PROG}_${type} -e h,static_library target=host-no-asserts;)
 	@touch ${PROG}_gen.exec
 
-$(foreach type,${LIST_TYPE},${PROG}_${type}.a): ${PROG}_gen.exec
+$(foreach type,${TYPE_LIST},${PROG}_${type}.a): ${PROG}_gen.exec
 
-$(foreach type,${LIST_TYPE},${PROG}_${type}.h): ${PROG}_gen.exec
+$(foreach type,${TYPE_LIST},${PROG}_${type}.h): ${PROG}_gen.exec
 
-${PROG}_test: ${PROG}_test.cc $(foreach type,${LIST_TYPE},${PROG}_${type}.h ${PROG}_${type}.a)
-	g++ -I . ${CXXFLAGS} $< -o $@ $(foreach type,${LIST_TYPE},${PROG}_${type}.a) -ldl -lpthread
+${PROG}_test: ${PROG}_test.cc $(foreach type,${TYPE_LIST},${PROG}_${type}.h ${PROG}_${type}.a)
+	g++ -I . ${CXXFLAGS} $< -o $@ $(foreach type,${TYPE_LIST},${PROG}_${type}.a) -ldl -lpthread
 
 ${PROG}_gen.hls: ${PROG}_generator.cc
 	g++ -D HALIDE_FOR_FPGA -fno-rtti ${CXXFLAGS} $< ${HALIDE_TOOLS_DIR}/GenGen.cpp -o ${PROG}_gen.hls ${LIBS} -lHalide
