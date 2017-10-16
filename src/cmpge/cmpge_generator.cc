d14d6b8a233539c3356b721b4449759eb8c0daaa
takashi.osawa <takashi.osawa@fixstars.com>
2017-10-16 11:14:25 +0900

    cmpge: support multiple type

--- src/cmpge/cmpge_generator.cc
+++ src/cmpge/cmpge_generator.cc
@@ -3,11 +3,12 @@
 
 using namespace Halide;
 
-class Cmpge : public Halide::Generator<Cmpge> {
+template<typename T>
+class Cmpge : public Halide::Generator<Cmpge<T>> {
 public:
     GeneratorParam<int32_t> width{"width", 1024};
     GeneratorParam<int32_t> height{"height", 768};
-    ImageParam src0{UInt(8), 2, "src0"}, src1{UInt(8), 2, "src1"};
+    ImageParam src0{type_of<T>(), 2, "src0"}, src1{type_of<T>(), 2, "src1"};
 
     Var x, y;
 
@@ -15,11 +16,13 @@ public:
 
         Func dst("dst");
         Expr srcval0 = src0(x, y), srcval1 = src1(x, y);
-        Expr dstval = cast<uint8_t>(select(srcval0 >= srcval1, type_of<uint8_t>().max(), 0));;
+        Expr dstval = cast<T>(select(srcval0 >= srcval1, type_of<T>().max(), 0));;
         dst(x, y) = dstval;
 
         return dst;
     }
 };
 
-RegisterGenerator<Cmpge> cmpge{"cmpge"};
+RegisterGenerator<Cmpge<uint8_t>> cmpge_u8{"cmpge_u8"};
+RegisterGenerator<Cmpge<uint16_t>> cmpge_u16{"cmpge_u16"};
+RegisterGenerator<Cmpge<uint32_t>> cmpge_u32{"cmpge_u32"};
