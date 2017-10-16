2f8c6e1914b07691d55faa2bd5c2ed5a55350bf9
takashi.osawa <takashi.osawa@fixstars.com>
2017-10-16 11:12:00 +0900

    cmpgt: support multiple type

--- src/cmpgt/cmpgt_generator.cc
+++ src/cmpgt/cmpgt_generator.cc
@@ -3,11 +3,12 @@
 
 using namespace Halide;
 
-class Cmpgt : public Halide::Generator<Cmpgt> {
+template<typename T>
+class Cmpgt : public Halide::Generator<Cmpgt<T>> {
 public:
     GeneratorParam<int32_t> width{"width", 1024};
     GeneratorParam<int32_t> height{"height", 768};
-    ImageParam src0{UInt(8), 2, "src0"}, src1{UInt(8), 2, "src1"};
+    ImageParam src0{type_of<T>(), 2, "src0"}, src1{type_of<T>(), 2, "src1"};
 
     Var x, y;
 
@@ -15,11 +16,13 @@ public:
 
         Func dst("dst");
         Expr srcval0 = src0(x, y), srcval1 = src1(x, y);
-        Expr dstval = cast<uint8_t>(select(srcval0 > srcval1, type_of<uint8_t>().max(), 0));
+        Expr dstval = cast<T>(select(srcval0 > srcval1, type_of<T>().max(), 0));
         dst(x, y) = dstval;
 
         return dst;
     }
 };
 
-RegisterGenerator<Cmpgt> cmpgt{"cmpgt"};
+RegisterGenerator<Cmpgt<uint8_t>> cmpgt_u8{"cmpgt_u8"};
+RegisterGenerator<Cmpgt<uint16_t>> cmpgt_u16{"cmpgt_u16"};
+RegisterGenerator<Cmpgt<uint32_t>> cmpgt_u32{"cmpgt_u32"};
