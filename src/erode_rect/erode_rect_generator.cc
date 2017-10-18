e0b72330bb7ccce783d683892decca5de37b9197
takashi.osawa <takashi.osawa@fixstars.com>
2017-10-18 15:33:17 +0900

    Revert "erode_rect: modify to use Param instead of GeneratorParam"
    This reverts commit b3db99905f262fd7c41a1d79e824958b458fd8fb.

--- src/erode_rect/erode_rect_generator.cc
+++ src/erode_rect/erode_rect_generator.cc
@@ -6,10 +6,10 @@ using namespace Halide;
 template<typename T>
 class ErodeRect : public Halide::Generator<ErodeRect<T>> {
 public:
+    GeneratorParam<int32_t> width{"width", 1024};
+    GeneratorParam<int32_t> height{"height", 768};
     GeneratorParam<int32_t> iteration{"iteration", 2};
     ImageParam src{type_of<T>(), 2, "src"};
-    Param<int32_t> width{"width", 1024};
-    Param<int32_t> height{"height", 768};
     Param<int32_t> window_width{"window_width", 3};
     Param<int32_t> window_height{"window_height", 3};
 
