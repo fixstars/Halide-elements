20acefcff2b6adbdfefeb9f5a23dea3b5c135821
Izumi Asakura <izumi.asakura@fixstars.com>
2017-10-20 17:19:20 +0900

    Add open_rect

---
+++ src/open_rect/open_rect_generator.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include "Halide.h"
+
+using namespace Halide;
+
+template<typename T>
+class OpenRect : public Halide::Generator<OpenRect<T>> {
+public:
+    GeneratorParam<int32_t> width{"width", 1024};
+    GeneratorParam<int32_t> height{"height", 768};
+    GeneratorParam<int32_t> iteration{"iteration", 2};
+    ImageParam src{type_of<T>(), 2, "src"};
+    Param<int32_t> window_width{"window_width", 3, 3, 17};
+    Param<int32_t> window_height{"window_height", 3, 3, 17};
+
+    Var x, y;
+
+    Func erode(Func src) {
+        Func input("input");
+        input(x, y) = src(x, y);
+
+        RDom r(-(window_width / 2), window_width, -(window_height / 2), window_height);
+        for (int32_t i = 0; i < iteration; i++) {
+            Func clamped = BoundaryConditions::repeat_edge(input, {{0, cast<int32_t>(width)}, {0, cast<int32_t>(height)}});
+            Func workbuf("workbuf");
+            Expr val = minimum(clamped(x + r.x, y + r.y));
+            workbuf(x, y) = val;
+            workbuf.compute_root();
+            input = workbuf;
+        }
+
+        return input;
+    }
+
+    Func dilate(Func src) {
+        Func input("input");
+        input(x, y) = src(x, y);
+
+        RDom r(-(window_width / 2), window_width, -(window_height / 2), window_height);
+        for (int32_t i = 0; i < iteration; i++) {
+            Func clamped = BoundaryConditions::repeat_edge(input, {{0, cast<int32_t>(width)}, {0, cast<int32_t>(height)}});
+            Func workbuf("workbuf");
+            Expr val = maximum(clamped(x + r.x, y + r.y));
+            workbuf(x, y) = val;
+            workbuf.compute_root();
+            input = workbuf;
+        }
+
+        return input;
+    }
+    
+    Func build() {
+        return dilate(erode(src));
+    }
+};
+
+RegisterGenerator<OpenRect<uint8_t>> open_rect_u8{"open_rect_u8"};
+RegisterGenerator<OpenRect<uint16_t>> open_rect_u16{"open_rect_u16"};
