15172bc20b66374d06430d073541ce8284d9d0a3
Izumi Asakura <izumi.asakura@fixstars.com>
2017-10-20 16:17:57 +0900

    Implement nand filter

---
+++ src/nand/nand_generator.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "Halide.h"
+
+using namespace Halide;
+
+template<typename T>
+class Nand : public Halide::Generator<Nand<T>> {
+public:
+    GeneratorParam<int32_t> width{"width", 1024};
+    GeneratorParam<int32_t> height{"height", 768};
+    ImageParam src0{type_of<T>(), 2, "src0"}, src1{type_of<T>(), 2, "src1"};
+
+    Var x, y;
+
+    Func build() {
+
+        Func dst("dst");
+        Expr dstval = ~(src0(x, y) & src1(x, y));
+        dst(x, y) = dstval;
+
+        return dst;
+    }
+};
+
+RegisterGenerator<Nand<uint8_t>> nand_u8{"nand_u8"};
+RegisterGenerator<Nand<uint16_t>> nand_u16{"nand_u16"};
+RegisterGenerator<Nand<uint32_t>> nand_u32{"nand_u32"};
