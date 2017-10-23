5087a2539f031d0a379183e5dc589cae51ad09d0
Izumi Asakura <izumi.asakura@fixstars.com>
2017-10-23 15:04:44 +0900

    Refactored

--- src/min_pos/min_pos_generator.cc
+++ src/min_pos/min_pos_generator.cc
@@ -5,7 +5,7 @@
 using namespace Halide;
 
 template<typename T>
-class Min_pos : public Halide::Generator<Min_pos<T>> {
+class MinPos : public Halide::Generator<MinPos<T>> {
 public:
     GeneratorParam<int32_t> width{"width", 1024};
     GeneratorParam<int32_t> height{"height", 768};
@@ -26,8 +26,8 @@ public:
     }
 };
 
-RegisterGenerator<Min_pos<uint8_t>> min_pos_u8{"min_pos_u8"};
-RegisterGenerator<Min_pos<uint16_t>> min_pos_u16{"min_pos_u16"};
-RegisterGenerator<Min_pos<uint32_t>> min_pos_u32{"min_pos_u32"};
-RegisterGenerator<Min_pos<float>> min_pos_f32{"min_pos_f32"};
-RegisterGenerator<Min_pos<double>> min_pos_f64{"min_pos_f64"};
+RegisterGenerator<MinPos<uint8_t>> min_pos_u8{"min_pos_u8"};
+RegisterGenerator<MinPos<uint16_t>> min_pos_u16{"min_pos_u16"};
+RegisterGenerator<MinPos<uint32_t>> min_pos_u32{"min_pos_u32"};
+RegisterGenerator<MinPos<float>> min_pos_f32{"min_pos_f32"};
+RegisterGenerator<MinPos<double>> min_pos_f64{"min_pos_f64"};
--- src/min_value/min_value_generator.cc
+++ src/min_value/min_value_generator.cc
@@ -5,7 +5,7 @@
 using namespace Halide;
 
 template<typename T>
-class Min_value : public Halide::Generator<Min_value<T>> {
+class MinValue : public Halide::Generator<MinValue<T>> {
 public:
     GeneratorParam<int32_t> width{"width", 1024};
     GeneratorParam<int32_t> height{"height", 768};
@@ -15,21 +15,18 @@ public:
     Var x{"x"}, y{"y"};
     
     Func build() {
-        Func ones("ones"), count("count"), dst("dst");        
+        Func count("count"), dst("dst");
         Var d("d");
 
-        ones(x, y) = 1;
         RDom r(0, width, 0, height, "r");
         r.where(roi(r.x, r.y) != 0);
-        count(d) = cast<int32_t>(0);
-        count(0) = sum(ones(r.x, r.y));
+        count() = sum(select(roi(r.x, r.y) == 0, 0, 1));
         
-        dst(d) = cast<T>(0);
-        dst(0) = cast<T>(select(count(0) == 0, 0, minimum(src(r.x, r.y))));
+        dst() = cast<T>(select(count() == 0, 0, minimum(src(r.x, r.y))));
         return dst;
     }
 };
 
-RegisterGenerator<Min_value<uint8_t>> min_value_u8{"min_value_u8"};
-RegisterGenerator<Min_value<uint16_t>> min_value_u16{"min_value_u16"};
-RegisterGenerator<Min_value<uint32_t>> min_value_u32{"min_value_u32"};
+RegisterGenerator<MinValue<uint8_t>> min_value_u8{"min_value_u8"};
+RegisterGenerator<MinValue<uint16_t>> min_value_u16{"min_value_u16"};
+RegisterGenerator<MinValue<uint32_t>> min_value_u32{"min_value_u32"};
