29caad4f2e7c23fcc1ad52ce29b3396d34a39926
iwag <iwagame@gmail.com>
2017-11-09 17:16:58 -0800

    reimplement 'nand' as the library style

--- include/Arithmetic.h
+++ include/Arithmetic.h
@@ -69,6 +69,19 @@ Halide::Func mul_scalar(Halide::Func src0, Halide::Expr val) {
     return dst;
 }
 
+template <typename T>
+Halide::Func nand(Halide::Func src0, Halide::Func src1) {
+    using namespace Halide;
+
+    Var x, y;
+    
+    Func dst("dst");
+    dst(x, y) = ~(src0(x, y) & src1(x, y));
+    
+    return dst;
+}
+
+
 }
 }
 
