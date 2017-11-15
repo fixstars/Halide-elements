64df1f60f63cc659549ed871f99b20b8244cffd6
Atsushi Kajita <kajita@fixstars.com>
2017-11-15 14:55:33 -0500

    added some operations to Complex.h. changed to use Complex structure.

--- include/Complex.h
+++ include/Complex.h
@@ -28,5 +28,21 @@ ComplexExpr operator*(const ComplexExpr& a, const ComplexExpr& b)
     return {a.x*b.x - a.y*b.y, a.x*b.y + a.y*b.x};
 }
 
+ComplexExpr operator/(const ComplexExpr& a, const Expr& b)
+{
+    return {a.x/b, a.y/b};
+}
+
+ComplexExpr conj(const ComplexExpr& a)
+{
+    return {a.x, -a.y};
+}
+
+ComplexExpr norm(const ComplexExpr& a)
+{
+    Expr len = sqrt(a.x*a.x + a.y*a.y);
+    return {a.x / len, a.y / len};
+}
+
 } // Element
 } // Halide
--- src/poc/poc_generator.cc
+++ src/poc/poc_generator.cc
@@ -17,12 +17,15 @@ public:
     Var c{"c"}, i{"i"}, k{"k"}, h{"h"};
     Var x{"x"}, y{"y"};
 
-    Func fft2(Func cmp1) {
+    ComplexExpr fft2(ComplexExpr a) {
         const int32_t n = static_cast<int32_t>(n_);
         const int32_t batch_size = static_cast<int32_t>(batch_size_);
 
+        Func cmpl_in("cmp_in");
+        cmpl_in(c, x, y) = select(c == 0, a.x, a.y);
+
         Func fftx("fftx");
-        fftx = fft(cmp1, n, batch_size);
+        fftx = fft(cmpl_in, n, batch_size);
 
         Func tr("tr");
         tr(c, x, y) = fftx(c, y, x);
@@ -30,74 +33,42 @@ public:
         Func ffty("ffty");
         ffty = fft(tr, n, batch_size);
 
-        return ffty;
+        ComplexExpr ret;
+        ret = {ffty(0, x, y), ffty(1, x, y)};
+
+        return ret;
     }
 
-    Func ifft2(Func a) {
+    ComplexExpr ifft2(ComplexExpr a) {
         const int32_t n = static_cast<int32_t>(n_);
 
-        Func a_conj, fft_a_conj, fft_a_conj2, ifft_a;
-
-        a_conj      = complex_conj(a);
-        fft_a_conj  = fft2(a_conj);
-        fft_a_conj2 = complex_conj(fft_a_conj);
-
-        ifft_a(c, x, y) = fft_a_conj2(c, x, y) / (n * n);
+        ComplexExpr fft_a_conj, ifft_a;
+        fft_a_conj = fft2(conj(a));
+        ifft_a     = conj(fft_a_conj) / (n * n);
 
         return ifft_a;
-    }
-
-    Func complex_conj(Func a) {
-        Func ret;
-        ret(c, i, k) = 0.0f;
-        ret(0, i, k) = a(0, i, k);
-        ret(1, i, k) = -a(1, i, k);
-
-        return ret;
-    }
-
-    Func complex_mul(Func a, Func b) {
-        Func ret("ret1");
-        ret(c, i, k) = 0.0f;
-        ret(0, i, k) = a(0, i, k) * b(0, i, k) - a(1, i, k) * b(1, i, k);
-        ret(1, i, k) = a(0, i, k) * b(1, i, k) + a(1, i, k) * b(0, i, k);
-
-        return ret;
-    }
-
-    Func complex_normal(Func a) {
-        Func len, ret;
-        len(i, k) = sqrt(a(0, i, k) * a(0, i, k) + a(1, i, k) * a(1, i, k));
-        ret(c, i, k) = a(c, i, k) / len(i, k);
-
-        return ret;
     }
 
     Func build() {
         const int32_t n = static_cast<int32_t>(n_);
 
-        Func w, hann_w_tmp, hann_w;
-        w(x, y) = sqrt((x / (float)n - 0.5f) * (x / (float)n - 0.5f) + (y / (float)n - 0.5f) * (y / (float)n - 0.5f));
-        hann_w_tmp(x, y) = 0.5f - 0.5f * cos(2.0f * (float)M_PI * ((w(x, y) + 0.5f)));
-        hann_w(x, y) = select(w(x, y) <= 0.5f, hann_w_tmp(x, y), 0.0f);
+        Expr w = sqrt((x / (float)n - 0.5f) * (x / (float)n - 0.5f) + (y / (float)n - 0.5f) * (y / (float)n - 0.5f));
+        Expr hann_w_tmp = 0.5f - 0.5f * cos(2.0f * (float)M_PI * ((w + 0.5f)));
+        Expr hann_w = select(w <= 0.5f, hann_w_tmp, 0.0f);
 
-        Func hann_w_in1, hann_w_in2;
-        hann_w_in1(c, x, y) = select(c==0, hann_w(x, y) * input1(x, y), 0);
-        hann_w_in2(c, x, y) = select(c==0, hann_w(x, y) * input2(x, y), 0);
+        ComplexExpr cmpl_hann_w1 = {hann_w * input1(x, y), 0};
+        ComplexExpr cmpl_hann_w2 = {hann_w * input2(x, y), 0};
 
-        Func fft_in1, fft_in2;
-        fft_in1 = fft2(hann_w_in1);
-        fft_in2 = fft2(hann_w_in2);
+        ComplexExpr cmpl_fft1 = fft2(cmpl_hann_w1);
+        ComplexExpr cmpl_fft2 = fft2(cmpl_hann_w2);
 
-        Func r, r_normal;
-        r = complex_mul(fft_in2, complex_conj(fft_in1));
-        r_normal = complex_normal(r);
+        ComplexExpr r = cmpl_fft2 * conj(cmpl_fft1);
+        ComplexExpr r_normal = norm(r);
 
-        Func ifft_r_n;
-        ifft_r_n = ifft2(r_normal);
+        ComplexExpr ifft_r_n = ifft2(r_normal);
 
         Func func_poc("func_poc");
-        func_poc(x, y) = ifft_r_n(0, x, y);
+        func_poc(x, y) = ifft_r_n.x;
 
         return func_poc;
     }
