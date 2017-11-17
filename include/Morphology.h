a44a736406d969f423e34a2fd633b882c44f9f79
iwag <iwagame@gmail.com>
2017-11-16 23:32:35 -0800

    reimplement 'erode_cross' as the library style

--- include/Morphology.h
+++ include/Morphology.h
@@ -145,6 +145,30 @@ Halide::Func erode(Halide::Func src, int32_t width, int32_t height, int32_t wind
     return input;
 }
 
+template<typename T>
+Halide::Func erode_cross(Halide::Func src, int32_t width, int32_t height, int32_t window_width, int32_t window_height, int32_t iteration)
+{
+    Var x, y;
+    Func input("input");
+    input(x, y) = src(x, y);
+
+    RDom r(-(window_width / 2), window_width, -(window_height / 2), window_height);
+    r.where(r.x == 0 || r.y == 0);
+    for (int32_t i = 0; i < iteration; i++) {
+        Func clamped = BoundaryConditions::repeat_edge(input, {{0, cast<int32_t>(width)}, {0, cast<int32_t>(height)}});
+        Func workbuf("workbuf");
+        Expr val = minimum_unroll(r, clamped(x + r.x, y + r.y));
+        workbuf(x, y) = val;
+        workbuf.compute_root();
+        input = workbuf;
+        schedule(workbuf, {width, height});
+    }
+
+    schedule(src, {width, height});
+
+    return input;
+}
+
 } // Element
 } // Halide
 
