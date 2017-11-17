778538e787dd5b486a960b7ee35bf0708a0bbeac
iwag <iwagame@gmail.com>
2017-11-16 23:26:37 -0800

    reimplement 'erode' as the library style

--- include/Morphology.h
+++ include/Morphology.h
@@ -114,6 +114,37 @@ Func conv_with_structure(Func src_img, std::function<Expr(RDom, Expr)> f, Expr i
     return input;
 }
 
+template<typename T>
+Halide::Func erode(Halide::Func src, int32_t width, int32_t height, int32_t window_width, int32_t window_height, Halide::Func structure, int32_t iteration)
+{
+    Var x, y;
+    Func input("input");
+    input(x, y) = src(x, y);
+
+    Func structure_("structure_");
+    structure_(x, y) = structure(x, y);
+    schedule(structure, {window_width, window_height});
+    schedule(structure_, {window_width, window_height});
+    
+    RDom r(-(window_width / 2), window_width, -(window_height / 2), window_height);
+    Func allzero("allzero");
+    allzero(x) = cast<bool>(true);
+    allzero(x) = allzero(x) && (structure_(r.x + window_width / 2, r.y + window_height / 2) == 0);
+    schedule(allzero, {1});
+    for (int32_t i = 0; i < iteration; i++) {
+        Func clamped = BoundaryConditions::repeat_edge(input, {{0, cast<int32_t>(width)}, {0, cast<int32_t>(height)}});
+        Func workbuf("workbuf");
+        Expr val = select(allzero(0), clamped(x - window_width / 2, y - window_height / 2), minimum_unroll(r, select(structure_(r.x + window_width / 2, r.y + window_height / 2) == 0, type_of<T>().max(), clamped(x + r.x, y + r.y))));
+        workbuf(x, y) = val;
+        schedule(workbuf, {width, height});
+        input = workbuf;
+    }
+
+    schedule(src, {width, height});
+
+    return input;
+}
+
 } // Element
 } // Halide
 
--- src/erode/erode_generator.cc
+++ src/erode/erode_generator.cc
@@ -16,34 +16,8 @@ public:
     GeneratorParam<int32_t> window_width{"window_width", 3, 3, 17};
     GeneratorParam<int32_t> window_height{"window_height", 3, 3, 17};
 
-    Var x, y;
-
     Func build() {
-
-        Func input("input");
-        input(x, y) = src(x, y);
-
-        Func structure_("structure_");
-        structure_(x, y) = structure(x, y);
-        schedule(structure, {window_width, window_height});
-        schedule(structure_, {window_width, window_height});
-        
-        RDom r(-(window_width / 2), window_width, -(window_height / 2), window_height);
-        Func allzero("allzero");
-        allzero(x) = cast<bool>(true);
-        allzero(x) = allzero(x) && (structure_(r.x + window_width / 2, r.y + window_height / 2) == 0);
-        schedule(allzero, {1});
-        for (int32_t i = 0; i < iteration; i++) {
-            Func clamped = BoundaryConditions::repeat_edge(input, {{0, cast<int32_t>(width)}, {0, cast<int32_t>(height)}});
-            Func workbuf("workbuf");
-            Expr val = select(allzero(0), clamped(x - window_width / 2, y - window_height / 2), minimum_unroll(r, select(structure_(r.x + window_width / 2, r.y + window_height / 2) == 0, type_of<T>().max(), clamped(x + r.x, y + r.y))));
-            workbuf(x, y) = val;
-            schedule(workbuf, {width, height});
-            input = workbuf;
-        }
-
-        schedule(src, {width, height});
-        return input;
+        return erode<T>(src, width, height, window_width, window_height, structure, iteration);
     }
 };
 
