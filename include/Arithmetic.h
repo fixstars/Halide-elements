bab8fd7562327e3d0ff65dbd73872218d0a87b87
takuya.iwakami <takuya.iwakami@fixstars.com>
2017-11-17 17:14:25 -0800

    reimplement 'cmpge' as the library style

--- include/Arithmetic.h
+++ include/Arithmetic.h
@@ -127,6 +127,18 @@ Halide::Func cmpgt(Halide::Func src0, Halide::Func src1) {
     Expr srcval0 = src0(x, y), srcval1 = src1(x, y);
     Expr dstval = cast<T>(select(srcval0 > srcval1, type_of<T>().max(), 0));
     dst(x, y) = dstval;
+    
+    return dst;
+}
+
+template <typename T>
+Halide::Func cmpge(Halide::Func src0, Halide::Func src1) {
+    Var x, y;
+    
+    Func dst("dst");
+    Expr srcval0 = src0(x, y), srcval1 = src1(x, y);
+    Expr dstval = cast<T>(select(srcval0 >= srcval1, type_of<T>().max(), 0));;
+    dst(x, y) = dstval;
 
     return dst;
 }
