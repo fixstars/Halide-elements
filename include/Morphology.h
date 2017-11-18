abac0a46e141196644ff9247eb3d0c395a6608a6
takuya.iwakami <takuya.iwakami@fixstars.com>
2017-11-17 16:13:12 -0800

    reimplement 'erode_rect' as the library style

--- include/Morphology.h
+++ include/Morphology.h
@@ -169,6 +169,28 @@ Halide::Func erode_cross(Halide::Func src, int32_t width, int32_t height, int32_
     return input;
 }
 
+template<typename T>
+Halide::Func erode_rect(Halide::Func src, int32_t width, int32_t height, int32_t window_width, int32_t window_height, int32_t iteration)
+{
+    Var x, y;
+    Func input("input");
+    input(x, y) = src(x, y);
+
+    RDom r(-(window_width / 2), window_width, -(window_height / 2), window_height);
+    for (int32_t i = 0; i < iteration; i++) {
+        Func clamped = BoundaryConditions::repeat_edge(input, {{0, cast<int32_t>(width)}, {0, cast<int32_t>(height)}});
+        Func workbuf("workbuf");
+        Expr val = minimum_unroll(r, clamped(x + r.x, y + r.y));
+        workbuf(x, y) = val;
+        workbuf.compute_root();
+        input = workbuf;
+        schedule(workbuf, {width, height});
+    }
+    schedule(src, {width, height});
+
+    return input;
+}
+
 } // Element
 } // Halide
 
