b44c65ce2a5da974dacdd1c4de4ca550a4b781f5
Akira Maruoka <akira.maruoka@fixstars.com>
2017-12-25 12:17:19 +0900

    Added cifar10 test.

--- include/NeuralNet.h
+++ include/NeuralNet.h
@@ -105,17 +105,20 @@ Func bin_conv(Func bottom, ImageParam weight, ImageParam alpha, ImageParam bias,
     RDom r(0, weight_shape[0], 0, weight_shape[1], 0, weight_shape[2]);
     const float elem_num = static_cast<float>(weight_shape[0] * weight_shape[1] * weight_shape[2]);
 
-    f(c, x, y, n) = (-elem_num +
-                     2 * sum(cast<float>(!in(r.x, x*stride - pad + r.y, y*stride - pad + r.z, n) ^
-                                                         weight(r.x, r.y, r.z, c)))) *
-        alpha(c) + bias(c);
+    if (pad == 0) {
+        f(c, x, y, n) = (-elem_num +
+                         2 * sum(cast<float>(!in(r.x, x*stride - pad + r.y, y*stride - pad + r.z, n) ^
+                                             weight(r.x, r.y, r.z, c)))) *
+            alpha(c) + bias(c);
+    } else {
+        Expr tx = x*stride - pad + r.y;
+        Expr ty = y*stride - pad + r.z;
 
-    top_shape = {
-        weight_shape[3],
-        (bottom_shape[1] - weight_shape[1] + 2*pad) / stride + 1,
-        (bottom_shape[2] - weight_shape[2] + 2*pad) / stride + 1,
-        bottom_shape[3]
-    };
+        Expr iw = !in(r.x, tx, ty, n) ^ weight(r.x, r.y, r.z, c);
+        f(c, x, y, n) = sum(r, select(tx >= 0 && tx < bottom_shape[1] && ty >= 0 && ty < bottom_shape[2],
+                                      cast<float>(select(iw, 1, -1)),
+                                      cast<float>(0))) * alpha(c) + bias(c);
+    }
 
     return f;
 }
@@ -151,7 +154,17 @@ Func bin_conv_fixed32(Func bottom, ImageParam weight, ImageParam alpha, ImagePar
     Fixed32 a = Fixed32{alpha(c)};
     Fixed32 b = Fixed32{bias(c)};
 
-    f(c, x, y, n) = static_cast<Expr>((-elem_num + (sum(r, to_fixed<int32_t, FB>(!v ^ w)) << 1)) * a + b);
+    if (pad == 0) {
+        f(c, x, y, n) = static_cast<Expr>((-elem_num + (sum(r, to_fixed<int32_t, FB>(!v ^ w)) << 1)) * a + b);
+    } else {
+        Expr tx = x*stride - pad + r.y;
+        Expr ty = y*stride - pad + r.z;
+
+        Expr iw = !v ^ w;
+        f(c, x, y, n) = static_cast<Expr>(sum(r, select(tx >= 0 && tx < bottom_shape[1] && ty >= 0 && ty < bottom_shape[2],
+                                                        to_fixed<int32_t, FB>(select(iw, 1, -1)),
+                                                        to_fixed<int32_t, FB>(0))) * a + b);
+    }
 
     top_shape = {
         weight_shape[3],
@@ -175,7 +188,7 @@ Func bin_conv_fixed32(Func bottom, ImageParam weight, ImageParam alpha, ImagePar
 
 
 // Pooling
-Func pool(Func bottom, const std::vector<int32_t>& window_shape, int32_t stride,
+Func pool(Func bottom, const std::vector<int32_t>& window_shape, int32_t stride, int32_t pad,
           const std::vector<int32_t>& bottom_shape, std::vector<int32_t>& top_shape)
 {
     Var x("x"), y("y"), c("c"), n("n");
@@ -187,20 +200,75 @@ Func pool(Func bottom, const std::vector<int32_t>& window_shape, int32_t stride,
 
     Func f;
     RDom r(0, window_shape[0], 0, window_shape[1]);
-    f(c, x, y, n) = maximum_unroll(r, in(c, x*stride + r.x, y*stride + r.y, n));
+    f(c, x, y, n) = maximum(r, in(c, x*stride - pad + r.x, y*stride - pad + r.y, n));
 
     top_shape = {
         bottom_shape[0],
-        (bottom_shape[1] - window_shape[0]) / stride + 1,
-        (bottom_shape[2] - window_shape[1]) / stride + 1,
+        (bottom_shape[1] - window_shape[0] + 2*pad) / stride + 1,
+        (bottom_shape[2] - window_shape[1] + 2*pad) / stride + 1,
         bottom_shape[3]
     };
 
     return f;
 }
 
+Func pool(Func bottom, const std::vector<int32_t>& window_shape, int32_t stride,
+          const std::vector<int32_t>& bottom_shape, std::vector<int32_t>& top_shape)
+{
+    const int32_t pad = 0;
+    return pool(bottom, window_shape, stride, pad, bottom_shape, top_shape);
+}
+
+Func avgpool(Func bottom, const std::vector<int32_t>& window_shape, int32_t stride, int32_t pad,
+             const std::vector<int32_t>& bottom_shape, std::vector<int32_t>& top_shape)
+{
+    Var x("x"), y("y"), c("c"), n("n");
+
+    Func in = BoundaryConditions::constant_exterior(bottom, 0,
+                                                    0, bottom_shape[0],
+                                                    0, bottom_shape[1],
+                                                    0, bottom_shape[2]);
+
+    Func f;
+    RDom r(0, window_shape[0], 0, window_shape[1]);
+    int32_t num = window_shape[0] * window_shape[1];
+    f(c, x, y, n) = sum(r, in(c, x*stride - pad + r.x, y*stride - pad + r.y, n)) / num;
+
+    top_shape = {
+        bottom_shape[0],
+        (bottom_shape[1] - window_shape[0] + 2*pad) / stride + 1,
+        (bottom_shape[2] - window_shape[1] + 2*pad) / stride + 1,
+        bottom_shape[3]
+    };
+
+    return f;
+}
+
+Func avgpool(Func bottom, const std::vector<int32_t>& window_shape, int32_t stride,
+             std::vector<int32_t>& bottom_shape, std::vector<int32_t>& top_shape)
+{
+    const int32_t pad = 0;
+    return avgpool(bottom, window_shape, stride, pad, bottom_shape, top_shape);
+}
+
+Func global_avgpool(Func bottom, const std::vector<int32_t>& bottom_shape, std::vector<int32_t>& top_shape)
+{
+    Var x("x"), y("y"), c("c"), n("n");
+
+    Func f;
+    const int32_t width = bottom_shape[1], height = bottom_shape[2];
+    RDom r(0, width, 0, height);
+    int32_t num = width * height;
+    f(c, n) = sum(r, bottom(c, r.x, r.y, n)) / num;
+
+    top_shape = {bottom_shape[0], bottom_shape[3]};
+
+    return f;
+}
+
+
 template<uint32_t FB>
-Func pool_fixed32(Func bottom, const std::vector<int32_t>& window_shape, int32_t stride,
+Func pool_fixed32(Func bottom, const std::vector<int32_t>& window_shape, int32_t stride, int32_t pad,
                   const std::vector<int32_t>& bottom_shape, std::vector<int32_t>& top_shape)
 {
     Var x("x"), y("y"), c("c"), n("n");
@@ -212,14 +280,77 @@ Func pool_fixed32(Func bottom, const std::vector<int32_t>& window_shape, int32_t
 
     Func f;
     RDom r(0, window_shape[0], 0, window_shape[1]);
-    f(c, x, y, n) = maximum_unroll(r, in(c, x*stride + r.x, y*stride + r.y, n));
+    f(c, x, y, n) = maximum(r, in(c, x*stride - pad + r.x, y*stride - pad + r.y, n));
 
     top_shape = {
         bottom_shape[0],
-        (bottom_shape[1] - window_shape[0]) / stride + 1,
-        (bottom_shape[2] - window_shape[1]) / stride + 1,
+        (bottom_shape[1] - window_shape[0] + 2*pad) / stride + 1,
+        (bottom_shape[2] - window_shape[1] + 2*pad) / stride + 1,
         bottom_shape[3]
     };
+
+    return f;
+}
+
+template<uint32_t FB>
+Func pool_fixed32(Func bottom, const std::vector<int32_t>& window_shape, int32_t stride,
+                  const std::vector<int32_t>& bottom_shape, std::vector<int32_t>& top_shape)
+{
+    const int32_t pad = 0;
+    return pool_fixed32<FB>(bottom, window_shape, stride, pad, bottom_shape, top_shape);
+}
+
+template<uint32_t FB>
+Func avgpool_fixed32(Func bottom, const std::vector<int32_t>& window_shape, int32_t stride, int32_t pad,
+                     const std::vector<int32_t>& bottom_shape, std::vector<int32_t>& top_shape)
+{
+    Var x("x"), y("y"), c("c"), n("n");
+
+    Func in = BoundaryConditions::constant_exterior(bottom, 0,
+                                                    0, bottom_shape[0],
+                                                    0, bottom_shape[1],
+                                                    0, bottom_shape[2]);
+    using Fixed32 = Fixed<int32_t, FB>;
+
+    Func f;
+    RDom r(0, window_shape[0], 0, window_shape[1]);
+    Fixed32 num = to_fixed<int32_t, FB>(window_shape[0] * window_shape[1]);
+    Fixed32 v = Fixed32{in(c, x*stride - pad + r.x, y*stride - pad + r.y, n)};
+    f(c, x, y, n) = static_cast<Expr>(sum(r, v) / num);
+
+    top_shape = {
+        bottom_shape[0],
+        (bottom_shape[1] - window_shape[0] + 2*pad) / stride + 1,
+        (bottom_shape[2] - window_shape[1] + 2*pad) / stride + 1,
+        bottom_shape[3]
+    };
+
+    return f;
+}
+
+template<uint32_t FB>
+Func avgpool_fixed32(Func bottom, const std::vector<int32_t>& window_shape, int32_t stride,
+                     std::vector<int32_t>& bottom_shape, std::vector<int32_t>& top_shape)
+{
+    const int32_t pad = 0;
+    return avgpool_fixed32<FB>(bottom, window_shape, stride, pad, bottom_shape, top_shape);
+}
+
+template<uint32_t FB>
+Func global_avgpool_fixed32(Func bottom, const std::vector<int32_t>& bottom_shape, std::vector<int32_t>& top_shape)
+{
+    Var x("x"), y("y"), c("c"), n("n");
+
+    using Fixed32 = Fixed<int32_t, FB>;
+
+    Func f;
+    const int32_t width = bottom_shape[1], height = bottom_shape[2];
+    RDom r(0, width, 0, height);
+    Fixed32 num = to_fixed<int32_t, FB>(width * height);
+    Fixed32 v = Fixed32{bottom(c, r.x, r.y, n)};
+    f(c, n) = static_cast<Expr>(sum(r, v) / num);
+
+    top_shape = {bottom_shape[0], bottom_shape[3]};
 
     return f;
 }
@@ -561,6 +692,82 @@ Func tofloat4d(Func bottom, const std::vector<int32_t>& bottom_shape, std::vecto
     top_shape = bottom_shape;
 
     return f;
+}
+
+
+Func binconv_module(Func bottom, std::vector<int32_t>& bottom_shape, const std::string& suffix,
+                    ImageParam bn_mean, ImageParam bn_variance, ImageParam bn_weight, ImageParam bn_bias,
+                    ImageParam conv_weight, ImageParam conv_alpha, ImageParam conv_bias, const std::vector<int32_t> conv_weight_shape,
+                    std::vector<int32_t>& top_shape)
+{
+    Var x("x"), y("y"), c("c"), n("n");
+
+    // Bn
+    Func bn_f("bn" + suffix);
+    std::vector<int32_t> bn_top_shape;
+    bn_f(c, x, y, n) = bn(bottom, bn_mean, bn_variance, bottom_shape, bn_top_shape)(c, x, y, n);
+
+    // Scale
+    Func scale_f("scale" + suffix);
+    std::vector<int32_t> scale_top_shape;
+    scale_f(c, x, y, n) = scale(bn_f, bn_weight, bn_bias, bn_top_shape, scale_top_shape)(c, x, y, n);
+
+    // BinActive
+    Func active_f("active" + suffix);
+    std::vector<int32_t> active_top_shape;
+    active_f(c, x, y, n) = bin_active(scale_f, scale_top_shape, active_top_shape)(c, x, y, n);
+    schedule(active_f, active_top_shape);
+
+    // Conv
+    Func conv_f("conv" + suffix);
+    std::vector<int32_t> conv_top_shape;
+    conv_f(c, x, y, n) =
+        bin_conv(active_f, conv_weight, conv_alpha, conv_bias, conv_weight_shape, active_top_shape, conv_top_shape)(c, x, y, n);
+
+    // ReLU:
+    Func relu_f("relu" + suffix);
+    std::vector<int32_t> relu_top_shape;
+    relu_f(c, x, y, n) = relu4d(conv_f, conv_top_shape, top_shape)(c, x, y, n);
+
+    return relu_f;
+}
+
+template <uint32_t FB>
+Func binconv_module_fixed32(Func bottom, std::vector<int32_t>& bottom_shape, const std::string& suffix,
+                            ImageParam bn_mean, ImageParam bn_variance, ImageParam bn_weight, ImageParam bn_bias,
+                            ImageParam conv_weight, ImageParam conv_alpha, ImageParam conv_bias, const std::vector<int32_t> conv_weight_shape,
+                            std::vector<int32_t>& top_shape)
+{
+    Var x("x"), y("y"), c("c"), n("n");
+
+    // Bn
+    Func bn_f("bn" + suffix);
+    std::vector<int32_t> bn_top_shape;
+    bn_f(c, x, y, n) = bn_fixed32<FB>(bottom, bn_mean, bn_variance, bottom_shape, bn_top_shape)(c, x, y, n);
+
+    // Scale
+    Func scale_f("scale" + suffix);
+    std::vector<int32_t> scale_top_shape;
+    scale_f(c, x, y, n) = scale_fixed32<FB>(bn_f, bn_weight, bn_bias, bn_top_shape, scale_top_shape)(c, x, y, n);
+
+    // BinActive
+    Func active_f("active" + suffix);
+    std::vector<int32_t> active_top_shape;
+    active_f(c, x, y, n) = bin_active_fixed32<FB>(scale_f, scale_top_shape, active_top_shape)(c, x, y, n);
+    schedule(active_f, active_top_shape);
+
+    // Conv
+    Func conv_f("conv" + suffix);
+    std::vector<int32_t> conv_top_shape;
+    conv_f(c, x, y, n) =
+        bin_conv_fixed32<FB>(active_f, conv_weight, conv_alpha, conv_bias, conv_weight_shape, active_top_shape, conv_top_shape)(c, x, y, n);
+
+    // ReLU:
+    Func relu_f("relu" + suffix);
+    std::vector<int32_t> relu_top_shape;
+    relu_f(c, x, y, n) = relu4d(conv_f, conv_top_shape, top_shape)(c, x, y, n);
+
+    return relu_f;
 }
 
 
---
+++ src/cifar10/cifar10_generator.cc
@@ -0,0 +1,253 @@
+#include <Halide.h>
+#include <Element.h>
+
+using namespace Halide;
+using namespace Halide::Element;
+
+class CIFAR10 : public Halide::Generator<CIFAR10> {
+    ImageParam in{Int(32), 4, "in"};
+    ImageParam c11w{Int(32), 4, "conv1_1_weight"};
+    ImageParam c11b{Int(32), 1, "conv1_1_bias"};
+    ImageParam bn11m{Int(32), 1, "bn1_1_mean"};
+    ImageParam bn11v{Int(32), 1, "bn1_1_variance"};
+    ImageParam bn12m{Int(32), 1, "bn1_2_mean"};
+    ImageParam bn12v{Int(32), 1, "bn1_2_variance"};
+    ImageParam bn12w{Int(32), 1, "bn1_2_weight"};
+    ImageParam bn12b{Int(32), 1, "bn1_2_bias"};
+    ImageParam c12w{Bool(), 4, "conv1_2_weight"};
+    ImageParam c12a{Int(32), 1, "conv1_2_alpha"};
+    ImageParam c12b{Int(32), 1, "conv1_2_bias"};
+    ImageParam bn13m{Int(32), 1, "bn1_3_mean"};
+    ImageParam bn13v{Int(32), 1, "bn1_3_variance"};
+    ImageParam bn13w{Int(32), 1, "bn1_3_weight"};
+    ImageParam bn13b{Int(32), 1, "bn1_3_bias"};
+    ImageParam c13w{Bool(), 4, "conv1_3_weight"};
+    ImageParam c13a{Int(32), 1, "conv1_3_alpha"};
+    ImageParam c13b{Int(32), 1, "conv1_3_bias"};
+    ImageParam bn21m{Int(32), 1, "bn2_1_mean"};
+    ImageParam bn21v{Int(32), 1, "bn2_1_variance"};
+    ImageParam bn21w{Int(32), 1, "bn2_1_weight"};
+    ImageParam bn21b{Int(32), 1, "bn2_1_bias"};
+    ImageParam c21w{Bool(), 4, "conv2_1_weight"};
+    ImageParam c21a{Int(32), 1, "conv2_1_alpha"};
+    ImageParam c21b{Int(32), 1, "conv2_1_bias"};
+    ImageParam bn22m{Int(32), 1, "bn2_2_mean"};
+    ImageParam bn22v{Int(32), 1, "bn2_2_variance"};
+    ImageParam bn22w{Int(32), 1, "bn2_2_weight"};
+    ImageParam bn22b{Int(32), 1, "bn2_2_bias"};
+    ImageParam c22w{Bool(), 4, "conv2_2_weight"};
+    ImageParam c22a{Int(32), 1, "conv2_2_alpha"};
+    ImageParam c22b{Int(32), 1, "conv2_2_bias"};
+    ImageParam bn23m{Int(32), 1, "bn2_3_mean"};
+    ImageParam bn23v{Int(32), 1, "bn2_3_variance"};
+    ImageParam bn23w{Int(32), 1, "bn2_3_weight"};
+    ImageParam bn23b{Int(32), 1, "bn2_3_bias"};
+    ImageParam c23w{Bool(), 4, "conv2_3_weight"};
+    ImageParam c23a{Int(32), 1, "conv2_3_alpha"};
+    ImageParam c23b{Int(32), 1, "conv2_3_bias"};
+    ImageParam bn31m{Int(32), 1, "bn3_1_mean"};
+    ImageParam bn31v{Int(32), 1, "bn3_1_variance"};
+    ImageParam bn31w{Int(32), 1, "bn3_1_weight"};
+    ImageParam bn31b{Int(32), 1, "bn3_1_bias"};
+    ImageParam c31w{Bool(), 4, "conv3_1_weight"};
+    ImageParam c31a{Int(32), 1, "conv3_1_alpha"};
+    ImageParam c31b{Int(32), 1, "conv3_1_bias"};
+    ImageParam bn32m{Int(32), 1, "bn3_2_mean"};
+    ImageParam bn32v{Int(32), 1, "bn3_2_variance"};
+    ImageParam bn32w{Int(32), 1, "bn3_2_weight"};
+    ImageParam bn32b{Int(32), 1, "bn3_2_bias"};
+    ImageParam c32w{Bool(), 4, "conv3_2_weight"};
+    ImageParam c32a{Int(32), 1, "conv3_2_alpha"};
+    ImageParam c32b{Int(32), 1, "conv3_2_bias"};
+    ImageParam bn33m{Int(32), 1, "bn3_3_mean"};
+    ImageParam bn33v{Int(32), 1, "bn3_3_variance"};
+    ImageParam c33w{Int(32), 4, "conv3_3_weight"};
+    ImageParam c33b{Int(32), 1, "conv3_3_bias"};
+
+    GeneratorParam<int32_t> batch_size{"batch_size", 100};
+
+public:
+    Func build()
+    {
+        Var x{"x"}, y{"y"}, c{"c"}, n{"n"};
+        const std::vector<int32_t> input_shape{3, 32, 32, batch_size};
+        constexpr uint32_t FB = 20;
+
+        Func input("input");
+        Fixed<int32_t, FB> mean = to_fixed<int32_t, FB>(select(c == 0, 0.4914f, c == 1, 0.4822f, 0.4465f));
+        Fixed<int32_t, FB> std = to_fixed<int32_t, FB>(select(c == 0, 0.2023f, c == 1, 0.1994f, 0.2010f));
+        input(c, x, y, n) = static_cast<Expr>((Fixed<int32_t, FB>{in(c, x, y, n)} - mean) / std);
+        schedule(input, input_shape);
+
+
+        // Conv1_1(3x5x5x192): (3, 32, 32, n) -> (192, 32, 32, n)
+        Func conv1_1("conv1_1");
+        const std::vector<int32_t> c11w_shape{3, 5, 5, 192};
+        std::vector<int32_t> conv1_1_top_shape;
+        conv1_1(c, x, y, n) = conv_fixed32<FB>(input, c11w, c11b, c11w_shape, input_shape, conv1_1_top_shape)(c, x, y, n);
+
+        // Bn1_1
+        Func bn1_1("bn1_1");
+        std::vector<int32_t> bn1_1_top_shape;
+        bn1_1(c, x, y, n) = bn_fixed32<FB>(conv1_1, bn11m, bn11v, conv1_1_top_shape, bn1_1_top_shape)(c, x, y, n);
+
+        // ReLU1_1:
+        Func relu1_1("relu1_1");
+        std::vector<int32_t> relu1_1_top_shape;
+        relu1_1(c, x, y, n) = relu4d(bn1_1, bn1_1_top_shape, relu1_1_top_shape)(c, x, y, n);
+
+        // Module1_2(192x1x1x160): (192, 32, 32, n) -> (160, 32, 32, n)
+        const std::vector<int32_t> c12w_shape{192, 1, 1, 160};
+        std::vector<int32_t> conv1_2_top_shape;
+        Func conv1_2 = binconv_module_fixed32<FB>(relu1_1, relu1_1_top_shape, "1_2",
+                                                  bn12m, bn12v, bn12w, bn12b, c12w, c12a, c12b, c12w_shape, conv1_2_top_shape);
+
+        // Module1_3(160x1x1x96): (160, 32, 32, n) -> (96, 32, 32, n)
+        const std::vector<int32_t> c13w_shape{160, 1, 1, 96};
+        std::vector<int32_t> conv1_3_top_shape;
+        Func conv1_3 = binconv_module_fixed32<FB>(conv1_2, conv1_2_top_shape, "1_3",
+                                                  bn13m, bn13v, bn13w, bn13b, c13w, c13a, c13b, c13w_shape, conv1_3_top_shape);
+        schedule(conv1_3, conv1_3_top_shape);
+
+        // Pool1(3x3, 2): (96, 32, 32, n) -> (96, 16, 16, n)
+        Func pool1("pool1");
+        std::vector<int32_t> pool1_top_shape;
+        pool1(c, x, y, n) = pool_fixed32<FB>(conv1_3, {3, 3}, 2, 1, conv1_3_top_shape, pool1_top_shape)(c, x, y, n);
+
+
+        // Module2_1(96x5x5x192): (96, 16, 16, n) -> (192, 16, 16, n)
+        const std::vector<int32_t> c21w_shape{96, 5, 5, 192};
+        std::vector<int32_t> conv2_1_top_shape;
+        Func conv2_1 = binconv_module_fixed32<FB>(pool1, pool1_top_shape, "2_1",
+                                                  bn21m, bn21v, bn21w, bn21b, c21w, c21a, c21b, c21w_shape, conv2_1_top_shape);
+
+        // Module2_2(192x1x1x192): (192, 16, 16, n) -> (192, 16, 16, n)
+        const std::vector<int32_t> c22w_shape{192, 1, 1, 192};
+        std::vector<int32_t> conv2_2_top_shape;
+        Func conv2_2 = binconv_module_fixed32<FB>(conv2_1, conv2_1_top_shape, "2_2",
+                                                  bn22m, bn22v, bn22w, bn22b, c22w, c22a, c22b, c22w_shape, conv2_2_top_shape);
+
+        // Module2_3(192x1x1x160): (192, 16, 16, n) -> (192, 16, 16, n)
+        const std::vector<int32_t> c23w_shape{192, 1, 1, 192};
+        std::vector<int32_t> conv2_3_top_shape;
+        Func conv2_3 = binconv_module_fixed32<FB>(conv2_2, conv2_2_top_shape, "2_3",
+                                                  bn23m, bn23v, bn23w, bn23b, c23w, c23a, c23b, c23w_shape, conv2_3_top_shape);
+        schedule(conv2_3, conv2_3_top_shape);
+
+        // Pool2(3x3, 2): (192, 16, 16, n) -> (192, 8, 8, n)
+        Func pool2("pool2");
+        std::vector<int32_t> pool2_top_shape;
+        pool2(c, x, y, n) = avgpool_fixed32<FB>(conv2_3, {3, 3}, 2, 1, conv2_3_top_shape, pool2_top_shape)(c, x, y, n);
+
+
+        // Module3_1(192x3x3x192): (192, 8, 8, n) -> (192, 8, 8, n)
+        const std::vector<int32_t> c31w_shape{192, 3, 3, 192};
+        std::vector<int32_t> conv3_1_top_shape;
+        Func conv3_1 = binconv_module_fixed32<FB>(pool2, pool2_top_shape, "3_1",
+                                                  bn31m, bn31v, bn31w, bn31b, c31w, c31a, c31b, c31w_shape, conv3_1_top_shape);
+
+        // Module3_2(192x1x1x192): (192, 8, 8, n) -> (192, 8, 8, n)
+        const std::vector<int32_t> c32w_shape{192, 1, 1, 192};
+        std::vector<int32_t> conv3_2_top_shape;
+        Func conv3_2 = binconv_module_fixed32<FB>(conv3_1, conv3_1_top_shape, "3_2",
+                                                  bn32m, bn32v, bn32w, bn32b, c32w, c32a, c32b, c32w_shape, conv3_2_top_shape);
+
+        // Bn3_3
+        Func bn3_3("bn3_3");
+        std::vector<int32_t> bn3_3_top_shape;
+        bn3_3(c, x, y, n) = bn_fixed32<FB>(conv3_2, bn33m, bn33v, conv3_2_top_shape, bn3_3_top_shape)(c, x, y, n);
+        schedule(bn3_3, bn3_3_top_shape);
+
+        // Conv3_3(192x1x1x10): (192, 8, 8, n) -> (10, 8, 8, n)
+        const std::vector<int32_t> c33w_shape{192, 1, 1, 10};
+        Func conv3_3("conv3_3");
+        std::vector<int32_t> conv3_3_top_shape;
+        conv3_3(c, x, y, n) = conv_fixed32<FB>(bn3_3, c33w, c33b, c33w_shape, bn3_3_top_shape, conv3_3_top_shape)(c, x, y, n);
+
+        // ReLU3_3:
+        Func relu3_3("relu3_3");
+        std::vector<int32_t> relu3_3_top_shape;
+        relu3_3(c, x, y, n) = relu4d(conv3_3, conv3_3_top_shape, relu3_3_top_shape)(c, x, y, n);
+        schedule(relu3_3, relu3_3_top_shape);
+
+        // Pool3(8x8, 1): (10, 8, 8, n) -> (10, 1, 1, n)
+        Func pool3("pool3");
+        std::vector<int32_t> pool3_top_shape;
+        pool3(c, n) = global_avgpool_fixed32<FB>(conv3_3, conv3_3_top_shape, pool3_top_shape)(c, n);
+
+        // tofloat:
+        Func tof("tof");
+        std::vector<int32_t> tof_top_shape;
+        tof(c, n) = tofloat2d<FB>(pool3, pool3_top_shape, tof_top_shape)(c, n);
+        schedule(tof, tof_top_shape);
+
+        schedule(in, input_shape);
+        schedule(c11w, c11w_shape);
+        schedule(c11b, {c11w_shape[3]});
+        schedule(bn11m, {c11w_shape[3]});
+        schedule(bn11v, {c11w_shape[3]});
+        schedule(bn12m, {c11w_shape[3]});
+        schedule(bn12v, {c11w_shape[3]});
+        schedule(bn12w, {c11w_shape[3]});
+        schedule(bn12b, {c11w_shape[3]});
+        schedule(c12w,  c12w_shape);
+        schedule(c12a,   {c12w_shape[3]});
+        schedule(c12b,   {c12w_shape[3]});
+        schedule(bn13m, {c12w_shape[3]});
+        schedule(bn13v, {c12w_shape[3]});
+        schedule(bn13w, {c12w_shape[3]});
+        schedule(bn13b, {c12w_shape[3]});
+        schedule(c13w,  c13w_shape);
+        schedule(c13a,   {c13w_shape[3]});
+        schedule(c13b,   {c13w_shape[3]});
+        schedule(bn21m, {c13w_shape[3]});
+        schedule(bn21v, {c13w_shape[3]});
+        schedule(bn21w, {c13w_shape[3]});
+        schedule(bn21b, {c13w_shape[3]});
+        schedule(c21w,  c21w_shape);
+        schedule(c21a,   {c21w_shape[3]});
+        schedule(c21b,   {c21w_shape[3]});
+        schedule(bn22m, {c21w_shape[3]});
+        schedule(bn22v, {c21w_shape[3]});
+        schedule(bn22w, {c21w_shape[3]});
+        schedule(bn22b, {c21w_shape[3]});
+        schedule(c22w,  c22w_shape);
+        schedule(c22a,   {c22w_shape[3]});
+        schedule(c22b,   {c22w_shape[3]});
+        schedule(bn23m, {c22w_shape[3]});
+        schedule(bn23v, {c22w_shape[3]});
+        schedule(bn23w, {c22w_shape[3]});
+        schedule(bn23b, {c22w_shape[3]});
+        schedule(c23w,  c23w_shape);
+        schedule(c23a,   {c23w_shape[3]});
+        schedule(c23b,   {c23w_shape[3]});
+        schedule(bn31m, {c23w_shape[3]});
+        schedule(bn31v, {c23w_shape[3]});
+        schedule(bn31w, {c23w_shape[3]});
+        schedule(bn31b, {c23w_shape[3]});
+        schedule(c31w,  c31w_shape);
+        schedule(c31a,   {c31w_shape[3]});
+        schedule(c31b,   {c31w_shape[3]});
+        schedule(bn32m, {c31w_shape[3]});
+        schedule(bn32v, {c31w_shape[3]});
+        schedule(bn32w, {c31w_shape[3]});
+        schedule(bn32b, {c31w_shape[3]});
+        schedule(c32w,  c32w_shape);
+        schedule(c32a,   {c32w_shape[3]});
+        schedule(c32b,   {c32w_shape[3]});
+        schedule(bn33m, {c32w_shape[3]});
+        schedule(bn33v, {c32w_shape[3]});
+        schedule(c33w,  c33w_shape);
+        schedule(c33b,   {c33w_shape[3]});
+
+        schedule(input, input_shape);
+        schedule(conv1_3, conv1_3_top_shape);
+        schedule(conv2_3, conv2_3_top_shape);
+        schedule(bn3_3, bn3_3_top_shape);
+        schedule(relu3_3, relu3_3_top_shape);
+        schedule(tof, tof_top_shape);
+
+        return tof;
+    }
+};
+HALIDE_REGISTER_GENERATOR(CIFAR10, "cifar10")
