f51c5aa4fe0beacd25db75b17ea8528d1247789d
momoko kono <momokok@localhost.localdomain>
2018-01-22 16:07:33 -0800

    moved to the end

--- include/Arithmetic.h
+++ include/Arithmetic.h
@@ -52,7 +52,6 @@ Func add(Func src0, Func src1)
     return dst;
 }
 
-
 template<typename T>
 Func add_scalar(Func src, Expr val)
 {
@@ -62,36 +61,6 @@ Func add_scalar(Func src, Expr val)
 
     Func dst;
     dst(x, y) = cast<T>(dstval);
-
-    return dst;
-}
-
-template<typename T>
-Func average_value(Func src, Func roi, int32_t width, int32_t height)
-{
-
-    Var x{"x"};
-    Func count{"count"}, dst{"dst"}; //umCheck{"sumcheck"};
-    RDom r{0, width, 0, height, "r"};
-    r.where(roi(r.x, r.y) != 0);
-
-
-
-
-
-    count(x) = sum(select(roi(r.x, r.y) == 0, 0, 1));
-
-    schedule(count, {1});
-    count.trace_stores();
-
-
-
-
-
-
-    dst(x) = cast<T>(select(count(x)==0, 0, sum(cast<double>(src(r.x, r.y)))/count(x)));
-
-
 
     return dst;
 }
@@ -365,6 +334,21 @@ Func histogram2d(Func src0, Func src1, int32_t width, int32_t height, int32_t hi
     Expr idx1 = cast<int32_t>(src1(r.x, r.y) * cast<uint64_t>(hist_width) / (cast<uint64_t>(type_of<T>().max()) + 1));
     dst(idx0, idx1) += cast<uint32_t>(1);
 
+    return dst;
+}
+
+template<typename T>
+Func average_value(Func src, Func roi, int32_t width, int32_t height)
+{
+    Var x{"x"};
+    Func count{"count"}, dst{"dst"}; //umCheck{"sumcheck"};
+    RDom r{0, width, 0, height, "r"};
+    r.where(roi(r.x, r.y) != 0);
+
+    count(x) = sum(select(roi(r.x, r.y) == 0, 0, 1));
+    //schedule(count, {1});
+
+    dst(x) = cast<T>(select(count(x)==0, 0, sum(cast<double>(src(r.x, r.y)))/count(x)));
     return dst;
 }
 
