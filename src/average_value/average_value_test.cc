234bb6a6827ca9f7e0f47dead9f059d0c7d94a1d
momoko kono <momokok@localhost.localdomain>
2018-01-22 16:10:36 -0800

    cleaned (the last one was made file

--- src/average_value/average_value_test.cc
+++ src/average_value/average_value_test.cc
@@ -16,14 +16,11 @@ using std::string;
 using std::vector;
 
 template <typename S, typename D>
-int test(int (*func)(struct halide_buffer_t *_src_buffer, struct halide_buffer_t *_roi_buffer, struct halide_buffer_t *_dst_buffer))
+int test(int (*func)(struct halide_buffer_t *_src_buffer,
+                    struct halide_buffer_t *_roi_buffer,
+                    struct halide_buffer_t *_dst_buffer))
 {
     try{
-        int ret = 0;
-
-        //
-        // Run
-        //
         const int32_t width = 1024;
         const int32_t height = 768;
         const std::vector<int32_t> extents{width, height};
@@ -31,24 +28,22 @@ int test(int (*func)(struct halide_buffer_t *_src_buffer, struct halide_buffer_t
         auto roi = mk_rand_buffer<uint8_t>(extents);
         auto output = mk_null_buffer<D>({1});
 
-func(input, roi, output);
+        func(input, roi, output);
         //reference
         D expect;
         double sum = 0;
         int count = 0;
         int els = 0;
-
         for (int j = 0; j < width; j++) {
             for (int i = 0; i < height; i++) {
                 if (roi(j, i) != 0) {
                     sum += input(j, i);
                     count++;
                 }
-
             }
         }
-
         expect = static_cast<D>(sum/count);
+
         D actual = output(0);
         if (expect != actual){
             throw std::runtime_error(format("Error: expect = %f, actual = %f\n",
@@ -67,7 +62,6 @@ int main(){
     #ifdef TYPE_u8_f32
         test<uint8_t, float>(average_value_u8_f32);
     #endif
-    printf("\n\n");
     #ifdef TYPE_u16_f32
         test<uint16_t, float>(average_value_u16_f32);
     #endif
