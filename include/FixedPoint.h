2af2cea3ee8b3132e664f1800567ff34f50324b0
Izumi Asakura <izumi.asakura@fixstars.com>
2018-01-24 19:20:48 +0900

    Add a fixed point type which takes bitwidth as a template parameter

--- include/FixedPoint.h
+++ include/FixedPoint.h
@@ -12,29 +12,37 @@ namespace Element {
 // Fixed point
 //
 
-template<typename BASE_T, uint32_t FB>
-struct Fixed {
+template<uint32_t NB, uint32_t FB, bool is_signed = true>
+struct FixedN {
     Expr v;
-    using upper_t = typename Upper<BASE_T>::type;
 
     explicit operator Expr() const {
         return v;
     }
 };
 
-template<typename BASE_T, uint32_t FB>
-inline Fixed<BASE_T, FB> to_fixed(Expr x)
+Type base_type(uint32_t nb, bool is_signed) {
+    return is_signed ? Int(nb) : UInt(nb);
+}
+
+Type upper_type(const Type& type) {
+    return type.with_bits(type.bits() * 2);
+}
+
+template<uint32_t NB, uint32_t FB, bool is_signed = true>
+inline FixedN<NB, FB, is_signed> to_fixed(Expr x)
 {
+    const Type& type = base_type(NB, is_signed);
     throw_assert(x.defined(), "conversion of undefined Expr");
     if (x.type().is_float()) {
-        return Fixed<BASE_T, FB>{cast<BASE_T>(x * Halide::Internal::make_const(type_of<BASE_T>(), 1<<FB))};
+        return FixedN<NB, FB>{cast(type, x * Halide::Internal::make_const(type, 1<<FB))};
     } else {
-        return Fixed<BASE_T, FB>{cast<BASE_T>(x) << FB};
+        return FixedN<NB, FB>{cast(type, x) << FB};
     }
 }
 
-template<typename T, typename BASE_T, uint32_t FB>
-inline Expr from_fixed(const Fixed<BASE_T, FB>& x)
+template<typename T, uint32_t NB, uint32_t FB>
+inline Expr from_fixed(const FixedN<NB, FB>& x)
 {
     throw_assert(x.v.defined(), "conversion of undefined Expr");
     if (type_of<T>().is_float()) {
@@ -44,148 +52,161 @@ inline Expr from_fixed(const Fixed<BASE_T, FB>& x)
     }
 }
 
-template<typename BASE_T, uint32_t FB>
-Fixed<BASE_T, FB> operator-(const Fixed<BASE_T, FB>& x)
+template<uint32_t NB, uint32_t FB>
+FixedN<NB, FB> operator-(const FixedN<NB, FB>& x)
 {
-    return Fixed<BASE_T, FB>{-x.v};
+    return FixedN<NB, FB>{-x.v};
 }
 
-template<typename BASE_T, uint32_t FB>
-Fixed<BASE_T, FB> operator+(const Fixed<BASE_T, FB>& x, const Fixed<BASE_T, FB>& y)
+template<uint32_t NB, uint32_t FB>
+FixedN<NB, FB> operator+(const FixedN<NB, FB>& x, const FixedN<NB, FB>& y)
 {
-    return Fixed<BASE_T, FB>{x.v + y.v};
+    return FixedN<NB, FB>{x.v + y.v};
 }
 
-template<typename BASE_T, uint32_t FB>
-Fixed<BASE_T, FB> operator-(const Fixed<BASE_T, FB>& x, const Fixed<BASE_T, FB>& y)
+template<uint32_t NB, uint32_t FB>
+FixedN<NB, FB> operator-(const FixedN<NB, FB>& x, const FixedN<NB, FB>& y)
 {
-    return Fixed<BASE_T, FB>{x.v - y.v};
+    return FixedN<NB, FB>{x.v - y.v};
 }
 
-template<typename BASE_T, uint32_t FB>
-Fixed<BASE_T, FB> operator*(const Fixed<BASE_T, FB>& x, const Fixed<BASE_T, FB>& y)
+template<uint32_t NB, uint32_t FB>
+FixedN<NB, FB> operator*(const FixedN<NB, FB>& x, const FixedN<NB, FB>& y)
 {
-    using UPPER_T = typename Fixed<BASE_T, FB>::upper_t;
-    return Fixed<BASE_T, FB>{cast<BASE_T>((cast<UPPER_T>(x.v) * cast<UPPER_T>(y.v)) >> FB)};
-    return Fixed<BASE_T, FB>{1024};
+    const Type& base_t = base_type(NB, FB);
+    const Type& upper_t = upper_type(base_t);
+    return FixedN<NB, FB>{cast(base_t, (cast(upper_t, x.v) * cast(upper_t, y.v)) >> FB)};
 }
 
-template<typename BASE_T, uint32_t FB>
-Fixed<BASE_T, FB> operator/(const Fixed<BASE_T, FB>& x, const Fixed<BASE_T, FB>& y)
+template<uint32_t NB, uint32_t FB>
+FixedN<NB, FB> operator/(const FixedN<NB, FB>& x, const FixedN<NB, FB>& y)
 {
-    using UPPER_T = typename Fixed<BASE_T, FB>::upper_t;
-    return Fixed<BASE_T, FB>{cast<BASE_T>((cast<UPPER_T>(x.v) << FB) / cast<UPPER_T>(y.v))};
+    const Type& base_t = base_type(NB, FB);
+    const Type& upper_t = upper_type(base_t);
+    return FixedN<NB, FB>{cast(base_t, (cast(upper_t, x.v) << FB) / cast(upper_t, y.v))};
 }
 
-template<typename BASE_T, uint32_t FB>
-Fixed<BASE_T, FB> operator<<(const Fixed<BASE_T, FB>& x, BASE_T y)
+template<uint32_t NB, uint32_t FB>
+FixedN<NB, FB> operator<<(const FixedN<NB, FB>& x, int32_t y)
 {
-    return Fixed<BASE_T, FB>{x.v << y};
+    return FixedN<NB, FB>{x.v << y};
 }
 
-template<typename BASE_T, uint32_t FB>
-Expr operator==(const Fixed<BASE_T, FB>& x, const Fixed<BASE_T, FB>& y)
+template<uint32_t NB, uint32_t FB>
+Expr operator==(const FixedN<NB, FB>& x, const FixedN<NB, FB>& y)
 {
     return x.v == y.v;
 }
 
-template<typename BASE_T, uint32_t FB>
-Expr operator!=(const Fixed<BASE_T, FB>& x, const Fixed<BASE_T, FB>& y)
+template<uint32_t NB, uint32_t FB>
+Expr operator!=(const FixedN<NB, FB>& x, const FixedN<NB, FB>& y)
 {
     return x.v != y.v;
 }
 
-template<typename BASE_T, uint32_t FB>
-Expr operator<(const Fixed<BASE_T, FB>& x, const Fixed<BASE_T, FB>& y)
+template<uint32_t NB, uint32_t FB>
+Expr operator<(const FixedN<NB, FB>& x, const FixedN<NB, FB>& y)
 {
     return x.v < y.v;
 }
 
-template<typename BASE_T, uint32_t FB>
-Expr operator<=(const Fixed<BASE_T, FB>& x, const Fixed<BASE_T, FB>& y)
+template<uint32_t NB, uint32_t FB>
+Expr operator<=(const FixedN<NB, FB>& x, const FixedN<NB, FB>& y)
 {
     return x.v <= y.v;
 }
 
-template<typename BASE_T, uint32_t FB>
-Expr operator>(const Fixed<BASE_T, FB>& x, const Fixed<BASE_T, FB>& y)
+template<uint32_t NB, uint32_t FB>
+Expr operator>(const FixedN<NB, FB>& x, const FixedN<NB, FB>& y)
 {
     return x.v > y.v;
 }
 
-template<typename BASE_T, uint32_t FB>
-Expr operator>=(const Fixed<BASE_T, FB>& x, const Fixed<BASE_T, FB>& y)
+template<uint32_t NB, uint32_t FB>
+Expr operator>=(const FixedN<NB, FB>& x, const FixedN<NB, FB>& y)
 {
     return x.v >= y.v;
 }
 
-template<typename BASE_T, uint32_t FB>
-Fixed<BASE_T, FB> floor(const Fixed<BASE_T, FB>& x)
+template<uint32_t NB, uint32_t FB>
+FixedN<NB, FB> floor(const FixedN<NB, FB>& x)
 {
-    constexpr BASE_T mask = ~((0x1 << FB) - 1);
-    Fixed<BASE_T, FB> zero = to_fixed<BASE_T, FB>(.0f);
-    Fixed<BASE_T, FB> one = to_fixed<BASE_T, FB>(1.0f);
-    return Fixed<BASE_T, FB>{select(x >= zero, x.v, (x - one).v + 1) & mask};
+    constexpr uint64_t mask = ~((0x1 << FB) - 1);
+    FixedN<NB, FB> zero = to_fixed<NB, FB>(.0f);
+    FixedN<NB, FB> one = to_fixed<NB, FB>(1.0f);
+    return FixedN<NB, FB>{select(x >= zero, x.v, (x - one).v + 1) & make_const(base_type(NB, FB), mask)};
 }
 
-template<typename BASE_T, uint32_t FB>
-Fixed<BASE_T, FB> min(const Fixed<BASE_T, FB>& x, const Fixed<BASE_T, FB>& y)
+template<uint32_t NB, uint32_t FB>
+FixedN<NB, FB> min(const FixedN<NB, FB>& x, const FixedN<NB, FB>& y)
 {
-    return Fixed<BASE_T, FB>{min(x.v, y.v)};
+    return FixedN<NB, FB>{min(x.v, y.v)};
 }
 
-template<typename BASE_T, uint32_t FB>
-Fixed<BASE_T, FB> max(const Fixed<BASE_T, FB>& x, const Fixed<BASE_T, FB>& y)
+template<uint32_t NB, uint32_t FB>
+FixedN<NB, FB> max(const FixedN<NB, FB>& x, const FixedN<NB, FB>& y)
 {
-    return Fixed<BASE_T, FB>{max(x.v, y.v)};
+    return FixedN<NB, FB>{max(x.v, y.v)};
 }
 
-template<typename BASE_T, uint32_t FB>
-Fixed<BASE_T, FB> clamp(const Fixed<BASE_T, FB>& x, const Fixed<BASE_T, FB>& min_val, const Fixed<BASE_T, FB>& max_val)
+template<uint32_t NB, uint32_t FB>
+FixedN<NB, FB> clamp(const FixedN<NB, FB>& x, const FixedN<NB, FB>& min_val, const FixedN<NB, FB>& max_val)
 {
-    return Fixed<BASE_T, FB>{clamp(x.v, min_val.v, max_val.v)};
+    return FixedN<NB, FB>{clamp(x.v, min_val.v, max_val.v)};
 }
 
-template<typename BASE_T, uint32_t FB>
-Fixed<BASE_T, FB> select(Expr condition, const Fixed<BASE_T, FB>& true_value, const Fixed<BASE_T, FB>& false_value)
+template<uint32_t NB, uint32_t FB>
+FixedN<NB, FB> select(Expr condition, const FixedN<NB, FB>& true_value, const FixedN<NB, FB>& false_value)
 {
-    return Fixed<BASE_T, FB>{select(condition, true_value.v, false_value.v)};
+    return FixedN<NB, FB>{select(condition, true_value.v, false_value.v)};
 }
 
-template<typename BASE_T, uint32_t FB, typename... Args>
-Fixed<BASE_T, FB> select(Expr c0, const Fixed<BASE_T, FB>& v0, Expr c1, const Fixed<BASE_T, FB>& v1, Args&&... args)
+template<uint32_t NB, uint32_t FB, typename... Args>
+FixedN<NB, FB> select(Expr c0, const FixedN<NB, FB>& v0, Expr c1, const FixedN<NB, FB>& v1, Args&&... args)
 {
-    return Fixed<BASE_T, FB>{select(c0, static_cast<Expr>(v0), static_cast<Expr>(select(c1, v1, std::forward<Args>(args)...)))};
+    return FixedN<NB, FB>{select(c0, static_cast<Expr>(v0), static_cast<Expr>(select(c1, v1, std::forward<Args>(args)...)))};
 }
 
-template<typename BASE_T, uint32_t FB>
-Fixed<BASE_T, FB> mac(RDom r, const Fixed<BASE_T, FB>& x, const Fixed<BASE_T, FB>& y)
+template<uint32_t NB, uint32_t FB>
+FixedN<NB, FB> mac(RDom r, const FixedN<NB, FB>& x, const FixedN<NB, FB>& y)
 {
-    using UPPER_T = typename Fixed<BASE_T, FB>::upper_t;
-    return Fixed<BASE_T, FB>{cast<BASE_T>(sum(r, cast<UPPER_T>(x.v) * cast<UPPER_T>(y.v)) >> FB)};
+    const Type& base_t = base_type(NB, FB);
+    const Type& upper_t = upper_type(base_t);
+    return FixedN<NB, FB>{cast(base_t, sum(r, cast(upper_t, x.v) * cast(upper_t, y.v)) >> FB)};
 }
 
-template<typename BASE_T, uint32_t FB>
-Fixed<BASE_T, FB> mac_unroll(RDom r, const Fixed<BASE_T, FB>& x, const Fixed<BASE_T, FB>& y)
+template<uint32_t NB, uint32_t FB>
+FixedN<NB, FB> mac_unroll(RDom r, const FixedN<NB, FB>& x, const FixedN<NB, FB>& y)
 {
-    using UPPER_T = typename Fixed<BASE_T, FB>::upper_t;
-    return Fixed<BASE_T, FB>{cast<BASE_T>(sum_unroll(r, cast<UPPER_T>(x.v) * cast<UPPER_T>(y.v)) >> FB)};
+    const Type& base_t = base_type(NB, FB);
+    const Type& upper_t = upper_type(base_t);
+    return FixedN<NB, FB>{cast(base_t, sum_unroll(r, cast(upper_t, x.v) * cast(upper_t, y.v)) >> FB)};
 }
 
-template<typename BASE_T, uint32_t FB>
-Fixed<BASE_T, FB> sum(RDom r, const Fixed<BASE_T, FB>& x)
+template<uint32_t NB, uint32_t FB>
+FixedN<NB, FB> sum(RDom r, const FixedN<NB, FB>& x)
 {
-    using UPPER_T = typename Fixed<BASE_T, FB>::upper_t;
-    return Fixed<BASE_T, FB>{cast<BASE_T>(sum(r, cast<UPPER_T>(x.v)))};
+    const Type& base_t = base_type(NB, FB);
+    const Type& upper_t = upper_type(base_t);
+    return FixedN<NB, FB>{cast(base_t, sum(r, cast(upper_t, x.v)))};
 }
 
-template<typename BASE_T, uint32_t FB>
-Fixed<BASE_T, FB> sum_unroll(RDom r, const Fixed<BASE_T, FB>& x)
+template<uint32_t NB, uint32_t FB>
+FixedN<NB, FB> sum_unroll(RDom r, const FixedN<NB, FB>& x)
 {
-    using UPPER_T = typename Fixed<BASE_T, FB>::upper_t;
-    return Fixed<BASE_T, FB>{cast<BASE_T>(sum_unroll(r, cast<UPPER_T>(x.v)))};
+    const Type& base_t = base_type(NB, FB);
+    const Type& upper_t = upper_type(base_t);
+    return FixedN<NB, FB>{cast(base_t, sum_unroll(r, cast(upper_t, x.v)))};
 }
 
+template<typename T, uint32_t FB>
+using Fixed = FixedN<sizeof(T) * 8, FB, std::is_signed<T>::value>;
+
+template<typename T, uint32_t FB>
+inline Fixed<T, FB> to_fixed(Expr x)
+{
+    return to_fixed<sizeof(T) * 8, FB, std::is_signed<T>::value>(x);
+}
 
 } //namespace Element
 } //namespace Halide
