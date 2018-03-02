3483ae456f1b7deb52c9c78058a6cebde6f5524e
Izumi Asakura <izumi.asakura@fixstars.com>
2018-03-02 15:52:10 +0900

    Port functions from halide's tests

--- include/FixedPoint.h
+++ include/FixedPoint.h
@@ -22,6 +22,37 @@ struct Fixed {
     explicit operator Expr() const {
         return v;
     }
+
+    static inline Fixed<BASE_T, FB> to_fixed(Expr x)
+    {
+         throw_assert(x.defined(), "conversion of undefined Expr");
+         if (x.type().is_float()) {
+             return Fixed<BASE_T, FB>{cast<BASE_T>(x * make_const(type_of<BASE_T>(), 1<<FB))};
+         } else {
+             return Fixed<BASE_T, FB>{cast<BASE_T>(x) << FB};
+         }
+    }
+
+    template<typename T> static inline Expr from_fixed(const Fixed<BASE_T, FB>&  x)
+    {
+        throw_assert(x.v.defined(), "conversion of undefined Expr");
+        if (type_of<T>().is_float()) {
+            return cast<T>(x.v) / cast<T>(1 << FB);
+        } else {
+            return cast<T>(x.v >> FB);
+        }
+    }
+
+    static inline Expr fixed_expr(Expr x)
+    {
+        Fixed<BASE_T, FB> fixed = to_fixed(x);
+        return static_cast<Expr>(fixed);
+    }
+
+    template<typename T> static inline Expr from_fixed_expr(Expr x)
+    {
+        return from_fixed<T>(Fixed<BASE_T, FB>{x});
+    }
 };
 
 template<typename BASE_T, uint32_t FB>
@@ -45,7 +76,32 @@ inline Expr from_fixed(const Fixed<BASE_T, FB>& x)
         return cast<T>(x.v >> FB);
     }
 }
+ 
+template<typename BASE_T, uint32_t FB>
+inline Expr fixed_expr(Expr x)
+{
+    Fixed<BASE_T, FB> fixed = to_fixed<BASE_T, FB>(x);
+    return static_cast<Expr>(fixed);
+}
 
+template<typename T, typename BASE_T, uint32_t FB>
+inline Expr from_fixed_expr(Expr x)
+{
+    return from_fixed<T>(Fixed<BASE_T, FB>{x});
+} 
+
+template<typename BASE_T, uint32_t FB>
+inline BASE_T to_fixed_base(float x)
+{
+    return static_cast<BASE_T>(x * (1<<FB));
+}
+
+template<typename T, typename BASE_T, uint32_t FB>
+inline T from_fixed(BASE_T x)
+{
+    return cast<T>(x) / cast<T>(1 << FB);
+}
+ 
 template<typename BASE_T, uint32_t FB>
 Fixed<BASE_T, FB> operator-(const Fixed<BASE_T, FB>& x)
 {
